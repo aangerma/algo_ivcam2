a77cfae7c468da9324ffe60bfb512389304e6f1f
GER\yotamshx <yotamx.shavit@intel.com>
2017-10-23 11:38:28 +0300

    separet losToolSlow error to axes

---
+++ scripts/LOSTool/losToolSlowAx.m
@@ -0,0 +1,62 @@
+function err = losToolSlowAx(varargin)
+p = inputHandler(varargin{:});
+fprintf('losToolSlowAx running on folder:\n\n%s\n\n', p.basedir)
+fprintf('%d ivs files found\n reading ivses...\n',length(p.ivsFilenames))
+ivsArr = cellfun(@(ivsFilename) io.readIVS(ivsFilename),p.ivsFilenames,'UniformOutput',0);
+calibIndx = max(1,min(p.calibImIndx,length(ivsArr)));
+fprintf('search for slow channel delay by ivs index %d\n%s\n\n',calibIndx,p.ivsFilenames{calibIndx})
+[~, slowChDelay] = Calibration.aux.mSyncerPipe(ivsArr{calibIndx},[],p.calibVerbose);
+fprintf('slow channel delay = %d\n', slowChDelay)
+fprintf('generet IR scan images...')
+irArr = cellfun(@(ivs) Utils.raw2slImg(ivs,slowChDelay),ivsArr,'UniformOutput',0);
+
+[imagePoints,bsz] = cellfun(@(ir) detectCheckerboardPoints(ir),irArr,'UniformOutput',0);
+
+if any(bsz{2:end} ~= bsz{1})
+    error('checker board point numbers not equal in all images!!!')
+end
+if isempty(bsz{1})
+    error('cant find checker board point!!!')
+end
+distfunc = @(v) max(abs([vec(real(v(:)-v(:).')), vec(imag(v(:)-v(:).'))]));
+ip=cell2mat(cellfun(@(x) (x(:,1)+1j*x(:,2)).',imagePoints,'uni',0));
+err = max(reshape(cell2mat(arrayfun(@(i) distfunc(ip(:,i)),1:size(ip,2),'UniformOutput',0)),size(ip)),[],2);
+fprintf('Scan Error X:\n\n%d\n\nScan Error Y:\n\n%d\n\n', err(1),err(2))
+if p.verbos
+end
+end
+
+
+function p = inputHandler(basedir,varargin)
+%defs
+defs.verbose = 1;
+defs.calibVerbose = 0;
+defs.calibImIndx = 1;
+defs.newFig = false;
+
+defs.calibfn = fullfile(basedir,filesep,'calib.csv');
+defs.configfn =fullfile(basedir,filesep,'config.csv');
+
+% varargin parse
+p = inputParser;
+
+isfile = @(x) exist(x,'file');
+isflag = @(x) or(isnumeric(x),islogical(x));
+
+addOptional(p,'verbose',defs.verbose,isflag);
+addOptional(p,'calibVerbose',defs.calibVerbose,isflag);
+addOptional(p,'calibFile',defs.calibfn,isfile);
+addOptional(p,'configFile',defs.configfn,isfile);
+addOptional(p,'calibImIndx',defs.calibImIndx,@isnumeric);
+addOptional(p,'newFig',defs.newFig,isflag);
+
+parse(p,varargin{:});
+
+p = p.Results;
+
+
+
+p.basedir = basedir;
+p.basedir(p.basedir == '"') = [];
+p.ivsFilenames = dirRecursive(p.basedir,'*.ivs');
+end
