1b02a47bab7e707ca3bbb0427be18805a8ce5463
GER\yotamshx <yotamx.shavit@intel.com>
2017-10-24 14:23:05 +0300

    create ir image in pixels instead of scanlines.

--- scripts/LOSTool/losToolSlowAx.m
+++ scripts/LOSTool/losToolSlowAx.m
@@ -8,22 +8,20 @@ fprintf('search for slow channel delay by ivs index %d\n%s\n\n',calibIndx,p.ivsF
 [~, slowChDelay] = Calibration.aux.mSyncerPipe(ivsArr{calibIndx},[],p.calibVerbose);
 fprintf('slow channel delay = %d\n', slowChDelay)
 fprintf('generet IR scan images...')
-irArr = cellfun(@(ivs) Utils.raw2slImg(ivs,slowChDelay),ivsArr,'UniformOutput',0);
+irArr = cellfun(@(ivs) Utils.raw2img(ivs,slowChDelay,[720,1280]),ivsArr,'UniformOutput',0);
 
 [imagePoints,bsz] = cellfun(@(ir) detectCheckerboardPoints(ir),irArr,'UniformOutput',0);
 
-if any(bsz{2:end} ~= bsz{1})
-    error('checker board point numbers not equal in all images!!!')
-end
 if isempty(bsz{1})
     error('cant find checker board point!!!')
 end
+if any(cellfun( @(bszi) any(bszi ~= bsz{1}),bsz(2:end)))
+    error('checker board point numbers not equal in all images!!!')
+end
 distfunc = @(v) max(abs([vec(real(v(:)-v(:).')), vec(imag(v(:)-v(:).'))]));
 ip=cell2mat(cellfun(@(x) (x(:,1)+1j*x(:,2)).',imagePoints,'uni',0));
-err = max(reshape(cell2mat(arrayfun(@(i) distfunc(ip(:,i)),1:size(ip,2),'UniformOutput',0)),size(ip)),[],2);
+err = max(reshape(cell2mat(arrayfun(@(i) distfunc(ip(:,i)),1:size(ip,2),'UniformOutput',0)),2,size(ip,2)),[],2);
 fprintf('Scan Error X:\n\n%d\n\nScan Error Y:\n\n%d\n\n', err(1),err(2))
-if p.verbos
-end
 end
 
 
@@ -43,7 +41,7 @@ p = inputParser;
 isfile = @(x) exist(x,'file');
 isflag = @(x) or(isnumeric(x),islogical(x));
 
-addOptional(p,'verbose',defs.verbose,isflag);
+% addOptional(p,'verbose',defs.verbose,isflag);
 addOptional(p,'calibVerbose',defs.calibVerbose,isflag);
 addOptional(p,'calibFile',defs.calibfn,isfile);
 addOptional(p,'configFile',defs.configfn,isfile);
