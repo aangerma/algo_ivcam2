43a412babf599a3179954e9cbc93e0d9a91778e9
GER\yotamshx <yotamx.shavit@intel.com>
2017-10-25 16:45:24 +0300

    add input verbose flag to losTool

--- scripts/LOSTool/losTool.m
+++ scripts/LOSTool/losTool.m
@@ -1,17 +1,21 @@
-function [errF, errSx, errSy] = losTool(baseDir)
-%mcc -m losTool.m -d \\ger\ec\proj\ha\perc\SA_3DCam\Ohad\share\POC4RangeFinder\ 
-verbose=true;
+function [errF, errSx, errSy] = losTool(baseDir,verbose)
+%mcc -m losTool.m -d \\ger\ec\proj\ha\perc\SA_3DCam\Ohad\share\POC4RangeFinder\
+if(~exist('verbose','var'))
+    verbose = false;
+end
 ivsFilenames = dirRecursive(baseDir,'*.ivs');
 
-
-fprintf('losToolSlowAx running on folder:\n\n%s\n\n', baseDir)
-fprintf('%d ivs files found\n reading ivses...\n',length(ivsFilenames))
+if verbose
+    fprintf('losToolSlowAx running on folder:\n\n%s\n\n', baseDir)
+    fprintf('%d ivs files found\n reading ivses...\n',length(ivsFilenames))
+end
 if length(ivsFilenames) < 2
     error('losTool need at least 2 ivses to run on');
 end
 ivsArr = cellfun(@(ivsFilename) io.readIVS(ivsFilename),ivsFilenames,'UniformOutput',0);
-
-fprintf('search for slow channel delay and fast error...');
+if verbose
+    fprintf('search for slow channel delay and fast error...');
+end
 warning off;
 [~,slowChDelay,errFArr] = cellfun(@(ivs)Calibration.aux.mSyncerPipe(ivs,[],verbose),ivsArr);
 warning on;
@@ -20,8 +24,10 @@ if any(slowChDelay(1:end-1) ~= slowChDelay(end))
 end
 [errF, minI] = nanmin(errFArr);
 slowChDelay = slowChDelay(minI);
-fprintf('slow channel delay = %d\n', slowChDelay)
-fprintf('generet IR images...')
+if verbose
+    fprintf('slow channel delay = %d\n', slowChDelay)
+    fprintf('generet IR images...')
+end
 sz = [1024 1024];
 irArr = cellfun(@(ivs) Utils.raw2img(ivs,slowChDelay,sz),ivsArr,'UniformOutput',0);
 
@@ -53,7 +59,16 @@ ip=cell2mat(cellfun(@(x) (x(:,1)+1j*x(:,2)),imagePoints,'uni',0)).';
 err = max(reshape(cell2mat(arrayfun(@(i) distfunc(ip(:,i)),1:size(ip,2),'UniformOutput',0)),2,size(ip,2)),[],2);
 errSx = err(1);
 errSy = err(2);
-fprintf('slow error X:\n%d\n\nslow error Y:\n%d\n\n fast error:\n%d\n', err(1),err(2))
+if verbose
+    fprintf('slow error X:\n%d\n\nslow error Y:\n%d\n\n fast error:\n%d\n', err(1),err(2))
+    figure(13531);
+    
+    pointsYoffset = find(~badRowsClean,1);
+    for i = 1:length(irArr)
+        figure(1000+i);imagesc(irArr{i});hold on;plot(ip(i,:)+1j*pointsYoffset,'*');hold off
+    end
+    figure(1000+i+1);plot(ip,'.')
+end
 end
 
 
