15f54b7cc137572be66fb1abe6623a954deecab6
Ohad <ohad.menashe@intel.com>
2017-10-30 12:41:25 +0200

    filter search based on  gradient descent

--- scripts/LOSTool/filterSearch.m
+++ scripts/LOSTool/filterSearch.m
@@ -2,22 +2,26 @@ function filterSearch()
 addpath('../POC4');
 bd = 'd:\data\lidar\EXP\20171025\';
 p = xml2structWrapper([bd 'pocConfigBase.xml']);
-x0 = p2x(p);
-xbest=fminsearch(@(x) calcLoss(bd,x),x0);
+
+x0=p2x(p);
+errFunc = @(x) calcLoss(bd,x);
+[xbest,eB]=gradientDecent(x0,errFunc,'xStep',50,'xTol',50,'verbose',true);
+disp(eB);
 pnew=x2p(xbest,p);
 struct2xmlWrapper(pnew,[bd 'pocConfigBest.xml']);
 
 end
 
 function err=calcLoss(bd,x)
+x = max(x,0);
 pold = xml2structWrapper([bd 'pocConfigBase.xml']);
 pnew=x2p(x,pold);
 struct2xmlWrapper(pnew,[bd 'pocConfig.xml']);
 try
-    aux.runPOCanalyzer(bd,false);%generate ivs
-    [errF, errSx, errSy] = losTool(bd);
+    ivsarr=aux.runPOCanalyzer(bd,false);%generate ivs
+    [errF, errSx, errSy] = losTool(ivsarr);
     err = mean([errF, errSx, errSy]);
-    fprintf('stepErr: %f\n',err);
+
 catch e,
     msgText = getReport(e);
     fprintf('ERROR: %s\n',msgText);
@@ -26,7 +30,7 @@ end
 end
 
 function x=p2x(p)
-x= [p.fa.filt(:)' p.sa.filt(:)' p.pa.filt(:)'];
+x= vec([p.fa.filt(:)' p.sa.filt(:)' p.pa.filt(:)']);
 end
 function p=x2p(x,p)
 fafilt = reshape(x(1:4),2,2);
