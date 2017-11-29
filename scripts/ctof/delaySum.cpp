fbc63e15f40e2b89b432d76ff748ffa81ffb4a05
GER\omenashe <Ohad@laptop>
2017-11-29 12:50:54 +0200

    bugfix

--- scripts/ctof/delaySum.cpp
+++ scripts/ctof/delaySum.cpp
@@ -3,16 +3,16 @@
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-	single* data = mxGetPr(prhs[0]);
-	single* delays = mxGetPr(prhs[1]);
-	single* attenuation = mxGetPr(prhs[2]);
+	const float* data = static_cast<const float*>(mxGetData(prhs[0]));
+	const float* delays = static_cast<const float*>(mxGetData(prhs[1]));
+	const float* attenuation = static_cast<const float*>(mxGetData(prhs[2]));
     
     
     int datalen = int(mxGetNumberOfElements(prhs[0]));
     int ndelays = int(mxGetNumberOfElements(prhs[1]));
 
 	plhs[0] = mxCreateDoubleMatrix(1, datalen,  mxREAL);
-	single* out = mxGetPr(plhs[0]);
+	float* out = static_cast<float*>(mxGetData(plhs[0]));
 
     for (int i = 0; i != ndelays; ++i)
 	{
