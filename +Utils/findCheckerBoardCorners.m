89191c8c281648f96953d8622376bf00e93027ff
GER\omenashe <ohad.menashe@intel.com>
2017-11-30 23:58:50 +0200

    bugfix

--- +Utils/findCheckerBoardCorners.m
+++ +Utils/findCheckerBoardCorners.m
@@ -29,8 +29,8 @@ v = pts-mean(pts);
 [u,~,~]=svd(v'*v);
 u=[sign(u(1)) 0 ; 0 sign(u(4))]*u;
 v = v*u;
-v=(v-min(v))./(max(v)-min(v)).*([bsz(2) bsz(1)]-0.01)+.5;
-%  v=(v-min(v))./(max(v)-min(v)).*([bsz(2) bsz(1)]-1)+1;
+% v=(v-min(v))./(max(v)-min(v)).*([bsz(2) bsz(1)]-0.01)+.5;
+  v=(v-min(v))./(max(v)-min(v)).*([bsz(2) bsz(1)]-1)+1;
 vI=round(v);
 
 for i=1:3
@@ -40,12 +40,15 @@ for i=1:3
     end
     tt=TPS(v(goodFit,:),vI(goodFit,:));
     vI(~goodFit,:)=round(tt.at(v(~goodFit,:)));
+    vI(:,1)=min(1,max(vI(:,1),bsz(2)));
+    vI(:,2)=min(1,max(vI(:,2),bsz(1)));
 end
 if(~all(goodFit))
     error('bad image formation');
 end
 
 if(verbose)
+    %%
     plot(v(:,1),v(:,2),'+',vI(:,1),vI(:,2),'g^');
     hold on;
     quiver(v(:,1),v(:,2),vI(:,1)-v(:,1),vI(:,2)-v(:,2),0)
@@ -57,8 +60,9 @@ end
 ind=(sub2ind(bsz,vI(:,2),vI(:,1)));
 [~,ordr]=sort(ind);
 p=pts(ordr,:);
-p=p(:,1)+1j*p(:,2);
+p=reshape(p(:,1)+1j*p(:,2),bsz);
 if(verbose)
+    %%
     imagesc(im)
     hold on;plot(p,'r+');hold off;
     arrayfun(@(i) text(real(p(i)),imag(p(i)),num2str(i),'color','g'),1:length(p))
