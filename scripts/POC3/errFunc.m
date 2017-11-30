00cdd71e810ecae669b6c64c631120de07fda861
Ohad <ohad.menashe@intel.com>
2017-11-30 13:13:16 +0200

    bugfix2

--- scripts/POC3/errFunc.m
+++ scripts/POC3/errFunc.m
@@ -1,12 +1,12 @@
 function e=errFunc(P,v,dt,params)
-
+warning('off',    'vision:calibrate:boardShouldBeAsymmetric');
 params.pzr2los = P;
 
 im=cellfun(@(x) scope2img(x,dt,params),v,'uni',0);
 indv=Utils.indx2col(size(im{1}),[3 3]);
 im = cellfun(@(x) reshape(nanmedian(x(indv)),size(im{1})),im,'uni',0);
 im = cellfun(@(x) nan2zero(x),im,'uni',0);
-im = cellfun(@(x) histeq(x),im,'uni',0);
+im = cellfun(@(x) histeq(normByMax(x)),im,'uni',0);
 try
     p = cellfun(@(x) detectCheckerboardPoints(x),im,'uni',0);
     imv=reshape([p{:}],size(p{1},1),size(p{1},2),[]);
