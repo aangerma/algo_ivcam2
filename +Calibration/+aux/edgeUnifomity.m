90ed990c16b87354bf2d994d95434137344a0600
GER\omenashe <ohad.menashe@intel.com>
2017-11-30 08:54:45 +0200

    tidy

--- +Calibration/+aux/edgeUnifomity.m
+++ +Calibration/+aux/edgeUnifomity.m
@@ -1,14 +1,17 @@
-function [err, distortionErr,checkerPoints] = edgeUnifomity(ir,tunnelWidth,horizontalMargin)
-EXPECTED_BOARD_SIZE=[10 14];
+function [err, distortionErr,pt] = edgeUnifomity(ir,tunnelWidth,horizontalMargin)
+ boardSize=[9 13];
 ir(sum(ir(:,2:end-1),2)==0,:)=[];
-warning('off','vision:calibrate:boardShouldBeAsymmetric')
-[imagePoints,boardSize] = detectCheckerboardPoints(ir);
-warning('on','vision:calibrate:boardShouldBeAsymmetric')
-if(~all(boardSize==EXPECTED_BOARD_SIZE))
-    error('Bad binput image/board size');
-end
-checkerPoints.x=reshape(imagePoints(:,1),boardSize-1);
-checkerPoints.y=reshape(imagePoints(:,2),boardSize-1);
+
+ir_=ir;
+ir_(isnan(ir_))=0;
+ir_=histeq(ir_);
+% pt = Utils.findCheckerBoardCorners(ir,boardSize,false);
+[pt,bsz]=detectCheckerboardPoints(ir_);
+pt=reshape(pt(:,1)+1j*pt(:,2),bsz-1);
+% if(~all(bsz-1==boardSize))
+%     error('Bad binput image/board size');
+% end
+% 
 
 
 
@@ -19,7 +22,7 @@ end
 if(~exist('horizontalMargin','var'))
     horizontalMargin=3;
 end
-checkerPoints.x=round(checkerPoints.x); %we are looking at scan lines - do not interp from neighbors
+%checkerPoints.x=round(checkerPoints.x); %we are looking at scan lines - do not interp from neighbors
 
 [yg,xg]=ndgrid(1:size(ir,1),1:size(ir,2));
 
@@ -28,24 +31,24 @@ checkerPoints.x=round(checkerPoints.x); %we are looking at scan lines - do not i
 xv={};
 yv={};
 yf={};
-evy=zeros(boardSize(1)-1,1);
-for i = 1:boardSize(1)-1
+evy=zeros(boardSize(1),1);
+for i = 1:boardSize(1)
     %find x location, minus vertical margin
-    xv{i} = setdiff(checkerPoints.x(i,1):checkerPoints.x(i,end),vec(checkerPoints.x(i,:)+(-horizontalMargin:horizontalMargin)'));
+    xv{i} = setdiff(real(pt(i,1)):real(pt(i,end)),vec(real(pt(i,:))+(-horizontalMargin:horizontalMargin)'));
     %find checkerboard y location in these point using splin interpolation
 
-    p=polyfit(checkerPoints.x(i,:),checkerPoints.y(i,:),2);
+    p=polyfit(real(pt(i,:)),imag(pt(i,:)),2);
     yv{i}=polyval(p,xv{i});
-    evy(i)=rms(polyval(p,checkerPoints.x(i,:))-checkerPoints.y(i,:));
+    evy(i)=rms(polyval(p,real(pt(i,:)))-imag(pt(i,:)));
 %          yv{i}=interp1(checkerPoints.x(i,:),checkerPoints.y(i,:),xv{i},'spline');
     %mark where checkerboard is top white bottom black, and vice versa
-    yf{i} = (-1)^i*interp1(checkerPoints.x(i,:),(-1).^(1:boardSize(2)-1),xv{i},'next');
+    yf{i} = (-1)^i*interp1(real(pt(i,:)),(-1).^(1:boardSize(2)),xv{i},'next');
     %interpolate
 end
-evx=zeros(boardSize(2)-1,1);
-for i = 1:boardSize(2)-1
-    p=polyfit(checkerPoints.y(:,i),checkerPoints.x(:,i),2);
-    evx(i)=rms(polyval(p,checkerPoints.y(:,i))-checkerPoints.x(:,i));
+evx=zeros(boardSize(2),1);
+for i = 1:boardSize(2)
+    p=polyfit(imag(pt(:,i)),real(pt(:,i)),2);
+    evx(i)=rms(polyval(p,imag(pt(:,i)))-real(pt(:,i)));
 end
 xv=[xv{:}];
 yv=[yv{:}];
@@ -60,7 +63,7 @@ if(0)
     subplot(5,5,setdiff(1:20,5:5:25));
     imagesc(ir); %#ok
      hold on;
-     plot(checkerPoints.x+1j*checkerPoints.y,'ro');
+     plot(pt,'ro');
      plot(xv,yv,'g.');
      hold off
      subplot(5,5,21:24);
@@ -68,7 +71,7 @@ if(0)
      subplot(5,5,25);
      plot(std(irBox,[],2),1:tunnelWidth*2+1);axis tight;set(gca,'ydir','reverse');
 end
-err = max(std(irBox,[],2));
+err = max(nanstd(irBox,[],2));
 end
 
 function err = errorFunc(ir)
