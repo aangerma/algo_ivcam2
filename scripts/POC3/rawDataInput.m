901cf6baaaabfc9b69f87fdd0fe433a937f66241
GER\omenashe <ohad.menashe@intel.com>
2017-12-05 09:24:27 +0200

    no message

--- scripts/POC3/rawDataInput.m
+++ scripts/POC3/rawDataInput.m
@@ -20,9 +20,11 @@ params.angxSO = [12 0];
 params.angySO = [2 0];
 params.slowSO = [29e3 0];
 %%
-params.pzr2los = [0.5248    0.0000    0.4942    0.1273    1.0214   -0.1234];
-% params.angxFilt(3) = 12089.7330567241;
-% params.angyFilt(3) = 57976.8520966172;
+%{
+ params.pzr2los = [0.5248    0.0000    0.4942    0.1273    1.0214   -0.1234];
+ params.angxFilt(3) = 12089.7330567241;
+ params.angyFilt(3) = 57976.8520966172;
+%}
 %% 
  [im,ivs_]=scope2img(v{1},dt,params);
  indv=Utils.indx2col(size(im),[3 3]);
