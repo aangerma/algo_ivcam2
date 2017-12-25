5ad5cecfdd3e77d34ecda430f62973d9e0a532e2
GER\omenashe <ohad.menashe@intel.com>
2017-12-25 17:15:09 +0200

    bugfix

--- common/@FirmwareBase/sprivRegstruct2uint32val.m
+++ common/@FirmwareBase/sprivRegstruct2uint32val.m
@@ -2,7 +2,7 @@ function v=sprivRegstruct2uint32val(s)
 v=zeros(length(s),1,'uint32');
 dvals = find([s.base]=='d');
 for i=dvals
-    v(i) = dec2hex(uint32(str2double(s(i).value)));
+    v(i) = uint32(str2double(s(i).value));
 end
 bvals = find([s.base]=='b');
 for i=bvals
