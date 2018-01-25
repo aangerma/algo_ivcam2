1e2d0ee7249afc84248972c8266e354543f95a01
Ohad <ohad.menashe@intel.com>
2018-01-25 11:41:43 +0200

    save to algonas, support for multi cpu

--- scripts/ctof/generateNNdata.m
+++ scripts/ctof/generateNNdata.m
@@ -1,5 +1,5 @@
 clear;
-N_SAMPLES=16e3;
+N_SAMPLES_PER_BATCH=1e2;
 sz = [24 32];
 % params.model = '+dataGen\+Shapes\flat.stl';
 
@@ -21,7 +21,7 @@ params.scenario.data{1}.txmod = [0 tw0 0];
 params.scenario.data{1}.rxmod = [0 tw1 0];
 
 %albedo
- P=randn([sz prod(sz)])>0;
+P=randn([sz prod(sz)])>0;
 %     P = reshape(eye(prod(sz)),sz(1),sz(2),[])>0;
 params.scenario.data{2}.pat=P;
 params.scenario.data{2}.txmod = [0 tw1 tw1 0 ];
@@ -38,22 +38,35 @@ params.sensor.sampler.v0 = 0;     %v
 params.sensor.sampler.v1 = 1e-3; %v
 params.sensor.collectionArea = 1;%mm^2
 
-%% 
-
-fid = fopen('ctofNNdata.bin','w');
-
-for i=1:N_SAMPLES
-tt=tic;
-params.model=dataGen.generateRandomSecene(i);
-[mes,gt]=Sim.run(params);
-imagesc(gt.a);
-drawnow;
-fwrite(fid,typecast([single([mes{:}]) single([gt.rtdS(:);gt.a(:)])'],'uint32'),'uint32');
-tt=toc(tt);
-fprintf('%d ( %5.2fsec)\n',i,tt);
+%%
+outputfldr = '\\algonas\Root\Data\cToF\simData';
+cnt = 0;
+while(true)
+    ifn = sprintf('%s\\simdataI_%06d.bin',outputfldr,cnt);
+    ofn = sprintf('%s\\simdataO_%06d.bin',outputfldr,cnt);
+    
+    if(~exist(ifn,'file'))
+        fidi=fopen(ifn,'w');
+        fido=fopen(ofn,'w');
+        for i=1:N_SAMPLES_PER_BATCH
+            tt= tic;
+            randseed = (cnt*N_SAMPLES_PER_BATCH+i-1);
+            params.model=dataGen.generateRandomSecene(randseed);
+            [mes,gt]=Sim.run(params);
+            mesv=single([mes{1} vec([mes{2};mes{3}])']);
+            fwrite(fidi,typecast(mesv,'uint32'),'uint32');
+            fwrite(fido,typecast(single([gt.rtdS(:);gt.a(:)])','uint32'),'uint32');
+            tt=toc(tt);
+            fprintf('Done(%4d sec) %5d %4d/%4d\n',round(tt),cnt,i,N_SAMPLES_PER_BATCH)
+        end
+        fclose(fidi);
+        fclose(fido);
+        
+    end
+    cnt=cnt+1;
 end
-fclose(fid);
 
-fid = fopen('ctofNNdata.cfg','w');
-fprintf(fid,'[data]\nnfeatures=%d\nheight=%d\nwidth=%d',length([mes{:}]),sz);
-fclose(fid);
+
+% fid = fopen(fullfile(outputfldr,filesep,'ctofNNdata.cfg'),'w');
+% fprintf(fid,'[data]\nnfeatures=%d\nheight=%d\nwidth=%d',length([mes{:}]),sz);
+% fclose(fid);
