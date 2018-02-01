6260a6ba30b66b0119b99a1f01438d5226eeada3
GER\omenashe <ohad.menashe@intel.com>
2018-02-01 12:18:41 +0200

    plot

--- +Calibration/+aux/undistFromImg.m
+++ +Calibration/+aux/undistFromImg.m
@@ -8,13 +8,20 @@ if(verbose)
     %%
     figure(sum(mfilename));
     clf;
+    subplot(121)
     imagesc(im);
+    axis image
     colormap gray
     hold on
     quiver(s(1,:),s(2,:),d(1,:)-s(1,:),d(2,:)-s(2,:));
     quiver(uxg,uyg,undistx,undisty);
     hold off
-    title(sprintf('Projective error: %f',e));
+    title(sprintf('Projective error: rms=%f,max=%f',e,max(sqrt(sum((d-s).^2,2)))));
+    subplot(122)
+    
+    quiver(s(1,:)*0,s(2,:)*0,d(1,:)-s(1,:),d(2,:)-s(2,:),0);
+    
+    axis square
 end
 
 end
