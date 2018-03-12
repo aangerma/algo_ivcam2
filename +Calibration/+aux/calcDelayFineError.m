3695fd9068acf8ee0696b78eb4741438edded3b4
Vitaly <vitaly.surazhsky@intel.com>
2018-03-12 13:49:14 +0200

    minor fix

--- +Calibration/+aux/calcDelayFineError.m
+++ +Calibration/+aux/calcDelayFineError.m
@@ -1,6 +1,6 @@
 function [err] = calcDelayFineError(img)
 
-errStat = Validation.edgeTrans(double(img), 7, [9 13]);
+errStat = Validation.edgeTrans(double(img), 9, [9 13]);
 err = errStat.vertMean;
 
 end
