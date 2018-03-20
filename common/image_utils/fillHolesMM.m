7e089ca7eaa71317a1afc1ceb7b6a9709d8b6ca4
Vitaly <vitaly.surazhsky@intel.com>
2018-03-20 15:04:47 +0200

    fillHolesMM fixed

--- common/image_utils/fillHolesMM.m
+++ common/image_utils/fillHolesMM.m
@@ -2,14 +2,15 @@ function [ filledI ] = fillHolesMM( I )
 
 Ip = padarray(I, [1 1], 'replicate', 'both');
 C = im2col(Ip, [3 3], 'sliding');
-CS = sort(C, 1);
 
-nValids = sum(CS ~= 0, 1);
+nValids = sum(C ~= 0, 1);
 ind1 = (nValids == 1);
 ind2 = (nValids == 2);
 ind3 = (nValids >= 3);
 
-Cv = zeros(size(I), class(I));
+CS = double(sort(C, 1));
+
+Cv = zeros(size(I));
 Cv(ind1) = CS(9,ind1);
 Cv(ind2) = (CS(8,ind2) + CS(9,ind2))/2;
 CS3 = CS(:,ind3);
@@ -21,7 +22,7 @@ C3 = CS3(sub2ind(size(CS3), mi+1, 1:size(CS3,2)));
 
 Cv(ind3) = (C1 + C3 + C2*2)/4;
 
-smI = reshape(Cv, size(I));
+smI = cast(reshape(Cv, size(I)), class(I));
 
 filledI = I;
 filledI(I == 0) = smI(I == 0);
