b0a3a295a0a6fa57149cd4345ca55f96458fc86e
GER\omenashe <ohad.menashe@intel.com>
2018-04-02 23:06:17 +0300

    -make setPath a function -close documents that are not part of the path

--- setPath_ivcam20.m
+++ setPath_ivcam20.m
@@ -1,7 +1,15 @@
-%%%%%%%%%
+function  setPath_ivcam20()
 restoredefaultpath;
 addpath(cd);
 p=strsplit(genpath(fullfile(cd,filesep,'common')),';');
 ishidden=cellfun(@(x) ~isempty(regexp(x,'\\\.[^\.]', 'once')),p);
 addpath(strjoin(p(~ishidden),';'));
-clear p ishidden RESTOREDEFAULTPATH_EXECUTED;
+
+
+%close open documents that are not part of the current path
+X = matlab.desktop.editor.getAll;
+X={X(cellfun(@(x) ~startsWith(x,cd),{X.Filename})).Filename};
+for x=X(:)'
+   matlab.desktop.editor.findOpenDocument(x{1}).close();
+end
+end
