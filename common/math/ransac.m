507fa9e11e274890682511abc0a2641a8c31a0d1
GER\omenashe <ohad.menashe@intel.com>
2018-04-11 10:11:35 +0300

    comments, plot func

--- common/math/ransac.m
+++ common/math/ransac.m
@@ -1,5 +1,18 @@
 function [bestInliers,bestModel] = ransac(X,generateModel,generateERR,varargin)
-
+%{
+general ransac iteration function
+-usage:
+    ransac(X,generateModel,generateERR,[name1],[value1],...)
+    X - Nxdim input data. N  is the number of data points, dim is the data dimintion
+    generateModel - function handle, recieve matrix of size nModelPoints x dim. sould return arbirtary model object
+    generateERR -  function handle, recieves arbirtary model object, and Mxdim data points, should return Mx1 error vector
+    optional(name value pair):
+     -iterations: number of RANSCAN iterations
+     -errorThr: threshold value for inlier/outlier separation
+     -nModelPoints: number of data point needed to build model
+    -seed:  random seed
+    
+%}
 
 %%%%%%%%%%%%%%%%%%%
 %varargin parser:
@@ -14,8 +27,9 @@ inp.addRequired('generateModel',@(x) isa(x,'function_handle') );
 inp.addRequired('generateERR',@(x) isa(x,'function_handle') );
 inp.addOptional('iterations', default_iterations, @(x)x > 0 && x < 10^5);
 inp.addOptional('errorThr', default_errorThr, @(x)x > 0 && x < 10^5);
+inp.addOptional('nModelPoints',size(X,2),@(x) isnumeric(x));
 inp.addOptional('seed',0,@(x) isnumeric(x));
-inp.addOptional('plotFunc','on',@(x)strcmp(x,'on') || strcmp(x,'off') );
+inp.addOptional('plot',false,@(x)islogical(x) );
 
 parse(inp,X,generateModel,generateERR,varargin{:});
 arg = inp.Results;
@@ -51,12 +65,12 @@ for i = 1:arg.iterations
 
 end
 
-if strcmp(arg.plotFunc, 'on')
+if(arg.plot)
     switch(dim)
         case 1
               plotModel = @(x,th,bi) plot(1:nSample,x(:,1),'.',bi,x(bi,2),'r.');
         case 2
-            plotModel = @(x,th,bi) plot(x(:,1),x(:,2),'.',x(bi,1),x(bi,2),'r.',x(:,1));
+            plotModel = @(x,th,bi) plot(x(:,1),x(:,2),'.',x(bi,1),x(bi,2),'.r');
    
         otherwise
             plotModel = @(x,th,bi) plot3(x(:,1),x(:,2),x(:,3),'b.',x(bi,1),x(bi,2),x(bi,3),'ro');
