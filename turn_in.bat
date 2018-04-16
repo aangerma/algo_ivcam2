7a1429a9a6282e2bbd224c37de302423112dc941
yresnick <yoni.resnick@intel.com>
2018-04-16 09:26:48 +0300

    avv: add new scripts

--- turn_in.bat
+++ turn_in.bat
@@ -1,8 +1,8 @@
 :: run script from hosting directory
 :: ..\..\..\algo_automation\tools\turn_in.py --repo-path ..\..\..\algo_ivcam2 --on-to-branch master --jenkins-job algo_ivcam2_Gated__Flow
 
-..\..\..\algo_automation\tools\turn_in.py --repo-path ..\..\..\algo_ivcam2 --on-to-branch master
+..\algo_automation\tools\turn_in.py --repo-path ..\algo_ivcam2 --on-to-branch master
 
 if %errorlevel%==0 (
-    ..\..\..\algo_automation\tools\gitMerge.py --repo-path ..\..\..\algo_ivcam2 --on-to-branch master
+    ..\algo_automation\tools\gitMerge.py --repo-path ..\algo_ivcam2 --on-to-branch master
     )
