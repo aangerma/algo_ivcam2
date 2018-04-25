cb0ae13e354c4a3f01bb4283b95def74cdd805ed
Vitaly <vitaly.surazhsky@intel.com>
2018-04-25 13:04:54 +0300

    fastCoarse with 0 gain

--- scripts/calibScripts/saveCMAsDelays.m
+++ scripts/calibScripts/saveCMAsDelays.m
@@ -7,7 +7,7 @@ end
 hw.setReg('RASTbiltBypass'     ,true);
 hw.setReg('RASTbiltSharpnessR'     ,uint8(0));
 hw.setReg('RASTbiltSharpnessS'     ,uint8(0));
-hw.setReg('JFILbypass$'         ,true);
+hw.setReg('JFILbypass$'         ,false);
 hw.setReg('JFILbilt1bypass'    ,true);
 hw.setReg('JFILbilt2bypass'    ,true);
 hw.setReg('JFILbilt3bypass'    ,true);
@@ -25,7 +25,7 @@ hw.setReg('JFILsort1bypassMode',uint8(0));
 hw.setReg('JFILsort2bypassMode',uint8(0));
 hw.setReg('JFILsort3bypassMode',uint8(1));
 hw.setReg('JFILupscalexyBypass',true);
-hw.setReg('JFILgammaBypass'    ,true);
+hw.setReg('JFILgammaBypass'    ,false);
 hw.setReg('DIGGsphericalEn',true);
 %hw.cmd('mwd a0020c00 a0020c04 01E00320 // DIGGsphericalScale'); % 01E00280
 %hw.cmd('mwd a0020bfc a0020c00 00f005E0 // DIGGsphericalOffset'); % 00f00500
@@ -41,12 +41,12 @@ initFastDelay = double(hw.readAddr('a0050548') + hw.readAddr('a0050458'));
 fastDelay = initFastDelay + 128 - initSlowDelay;
 Calibration.aux.hwSetDelay(hw, fastDelay, true);
 
-dRange = 0; %128;
+dRange = 128;
 delays = fastDelay-dRange:4:fastDelay+dRange;
 %delays = 29000:4:29100;
 nDelays = length(delays);
 
-frames = cell(nDelays, 4);
+frames = cell(nDelays, 5);
 
 tmplLength = double(hw.read('GNRLtmplLength'));
 
@@ -55,6 +55,8 @@ tmplLength = double(hw.read('GNRLtmplLength'));
 frame = hw.getFrame();
 imSize = size(frame.i);
 
+hwSetScanDir(hw, 2);
+
 for i=1:nDelays
     delay = delays(i);
     Calibration.aux.hwSetDelay(hw, delay, true);
@@ -62,29 +64,48 @@ for i=1:nDelays
     slowDelay = 128 + delay - fastDelay;
     Calibration.aux.hwSetDelay(hw, slowDelay, false);
 
+    hw.setReg('JFILbypass$', false);
     hw.setReg('DCORoutIRcma$', false);
     hw.shadowUpdate();
-    pause(0.2);
+    hwSetScanDir(hw, 2);
     frames{i,1} = hw.getFrame(30); % ir frame
+    hwSetScanDir(hw, 0);
+    frames{i,2} = hw.getFrame(30); % ir frame
+    hwSetScanDir(hw, 1);
+    frames{i,3} = hw.getFrame(30); % ir frame
 
-    cma = zeros([tmplLength size(frames{i,1}.i)], 'uint8');
-    for iCMA = (1:20)-1 %tmplLength
+    cma0 = zeros([tmplLength size(frames{i,1}.i)], 'uint8');
+    cma1 = zeros([tmplLength size(frames{i,1}.i)], 'uint8');
+    for iCMA = (1:tmplLength)-1
+        hw.setReg('JFILbypass$', true);    
         hw.setReg('DCORoutIRcma$', true);
-        strCmdIndex = 'mwd a00208c8 a00208cc 0000%02u%02u // DCORoutIRcmaIndex';
+        
         %hw.setReg('DCORoutIRcmaIndex', [uint8(floor(iCMA/84)) uint8(floor(mod(iCMA,84)))]);
+        
+      
+        strCmdIndex = 'mwd a00208c8 a00208cc 0000%02x%02x // DCORoutIRcmaIndex';
         hw.cmd(sprintf(strCmdIndex, uint8(floor(mod(iCMA,84))), uint8(floor(iCMA/84))));
         hw.shadowUpdate();
-        pause(0.2);
-        [cmaBin, imgBin] = getBin(hw, 0, imSize);
-        cma(iCMA+1,:,:) = cmaBin;
-        
+
+        hwSetScanDir(hw, 0);
+        [cmaBin0, cmaC0] = getBin(hw, 6, imSize);
+        cma0(iCMA+1,:,:) = cmaBin0;
+
+        hwSetScanDir(hw, 1);
+        [cmaBin1, cmaC1] = getBin(hw, 6, imSize);
+        cma1(iCMA+1,:,:) = cmaBin1;
+
         figure(11711);        
-        imagesc(imgBin);
+        subplot(2,2,1); imagesc(cmaBin0);
+        subplot(2,2,3); imagesc(cmaBin1);
+        subplot(2,2,2); imagesc(cmaC0);
+        subplot(2,2,4); imagesc(cmaC1);
         tStr = sprintf('Delays %u of %u, bin %u of %u', i, nDelays, iCMA, tmplLength);
-        title(tStr);
+        subplot(2,2,1); title(tStr);
         drawnow;
     end
-    frames{i,2} = cma;
+    frames{i,4} = cma0;
+    frames{i,5} = cma1;
     
     %{
     figure(11711); 
@@ -101,6 +122,7 @@ end
 hw.setReg('DCORoutIRcma$', false);
 hw.setReg('JFILbypass$', false);
 hw.shadowUpdate();
+hwSetScanDir(hw, 2);
 
 Calibration.aux.hwSetDelay(hw, initSlowDelay, false);
 Calibration.aux.hwSetDelay(hw, initFastDelay, true);
@@ -112,48 +134,22 @@ Calibration.aux.hwSetDelay(hw, initFastDelay, true);
 
 end
 
-function [cmaBin, img] = getBin(hw, nExp, imSize)
+function [cmaBin, cmaC] = getBin(hw, nExp, imSize)
 
-cmaBin = zeros(imSize, 'uint16');
+cmaA = zeros(imSize);
+cmaC = zeros(imSize);
 
 
 
 for i=1:2^nExp
-    img = hw.getFrame().i;
-    %frameCMAu64 = byteToBoolLut(img(:)+1);
-    %bins = reshape(typecast(frameCMAu64,'uint8'), [8 imSize]);
-    cmaBin = cmaBin + uint16(img);
+    frame = hw.getFrame();
+    cmaA = cmaA + double(frame.i);
+    cmaC = cmaC + double(frame.z ~= 0);
 end
 
-cmaBin = bitshift(cmaBin, -nExp+2);
+cmaBin = uint8(cmaA * 4 ./ cmaC);
+cmaC = uint8(cmaC);
 
 end
 
-
-function [] = hwSetScanDir(hw, dir)
-
-scanDir0gainAddr = '85080000';
-scanDir1gainAddr = '85080480';
-gainCalibValue  = '000ffff0';
-
-global saveVal;
-if isempty(saveVal)
-    saveVal(1) = hw.readAddr(scanDir0gainAddr);
-    saveVal(2) = hw.readAddr(scanDir1gainAddr);
-end
-
-if (dir == 0)
-    hw.writeAddr(scanDir0gainAddr,gainCalibValue,true);
-    hw.writeAddr(scanDir1gainAddr,saveVal(2),true);
-elseif (dir == 1)
-    hw.writeAddr(scanDir1gainAddr,gainCalibValue,true);
-    hw.writeAddr(scanDir0gainAddr,saveVal(1),true);
-elseif (dir == 2)
-    hw.writeAddr(scanDir0gainAddr,saveVal(1),true);
-    hw.writeAddr(scanDir1gainAddr,saveVal(2),true);
-end
-
-pause(0.15);
-
-end
 
