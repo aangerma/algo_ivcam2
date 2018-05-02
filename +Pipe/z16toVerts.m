352e1a0895f712e834a891786c7867c6f491fafe
ohadmen <ohad.menashe@intel.com>
2018-05-03 00:17:31 +0300

    use intrinsics in reconstruction

--- +Pipe/z16toVerts.m
+++ +Pipe/z16toVerts.m
@@ -5,7 +5,7 @@ z = double(zUINT16)/bitshift(1,regs.GNRL.zMaxSubMMExp);
 z(zUINT16==0)=nan;
 [v,u]=ndgrid(0:sz(1)-1,0:sz(2)-1);
 
-matK=reshape([typecast(regs.DCOR.spare,'single') 1],3,3)';
+matK=reshape([typecast(regs.CBUF.spare,'single') 1],3,3)';
 matKi=matK^-1;
 tt=z(:)'.*[u(:)';v(:)';ones(1,numel(v))];
 verts=reshape((matKi*tt)',[sz 3]);
