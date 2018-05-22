b5ad403c64e4b5cdd36ae4029a8ed4b08061ffd8
ohadmen <ohad.menashe@intel.com>
2018-05-22 15:31:18 +0300

    explode filenames onto specific addition

--- @HWinterface/burn2device.m
+++ @HWinterface/burn2device.m
@@ -17,35 +17,32 @@ function burn2device(obj,basedir,burnCalib,burnConfig)
     end
     filenamesTableIndexLUT={};
     if(burnCalib)
-    filenamesTableIndexLUT(end+1:end+4,:)=   {
-        'WrCalibData' ,'Algo_Pipe_Calibration_VGA_CalibData_Ver_*.txt'          ,'0a0';
-        'WrCalibInfo' ,'DIGG_Gamma_Info_CalibInfo_Ver_*.bin'                    ,'030';
-        'WrCalibInfo' ,'DIGG_Undist_Info_1_CalibInfo_Ver_*.bin'                 ,'040';
-        'WrCalibInfo' ,'DIGG_Undist_Info_2_CalibInfo_Ver_*.bin'                 ,'041';
-        };
+        filenamesTableIndexLUT(end+1,:)={ 'WrCalibData' ,'Algo_Pipe_Calibration_VGA_CalibData_Ver_*.txt'          ,'0a0'};
+        filenamesTableIndexLUT(end+1,:)={ 'WrCalibInfo' ,'DIGG_Gamma_Info_CalibInfo_Ver_*.bin'                    ,'030'};
+        filenamesTableIndexLUT(end+1,:)={ 'WrCalibInfo' ,'DIGG_Undist_Info_1_CalibInfo_Ver_*.bin'                 ,'040'};
+        filenamesTableIndexLUT(end+1,:)={ 'WrCalibInfo' ,'DIGG_Undist_Info_2_CalibInfo_Ver_*.bin'                 ,'041'};
     end
     if(burnConfig)
-        filenamesTableIndexLUT(end+1:end+15,:)=  {
-            'WrConfigData','Algo_Dynamic_Configuration_VGA30_1_ConfigData_Ver_*.txt','133';
-            'WrConfigData','Algo_Dynamic_Configuration_VGA30_2_ConfigData_Ver_*.txt','134';
-%           'WrConfigData','Algo_Dynamic_Configuration_VGA30_3_ConfigData_Ver_*.txt','135';
-            'WrConfigInfo','DCOR_cml_0_Info_ConfigInfo_Ver_*.bin'                   ,'180';
-            'WrConfigInfo','DCOR_cml_1_Info_ConfigInfo_Ver_*.bin'                   ,'181';
-            'WrConfigInfo','DCOR_cml_2_Info_ConfigInfo_Ver_*.bin'                   ,'18B';
-            'WrConfigInfo','DCOR_fml_0_Info_ConfigInfo_Ver_*.bin'                   ,'182';
-            'WrConfigInfo','DCOR_fml_1_Info_ConfigInfo_Ver_*.bin'                   ,'183';
-            'WrConfigInfo','DCOR_fml_2_Info_ConfigInfo_Ver_*.bin'                   ,'184';
-            'WrConfigInfo','DCOR_fml_3_Info_ConfigInfo_Ver_*.bin'                   ,'185';
-            'WrConfigInfo','DCOR_fml_4_Info_ConfigInfo_Ver_*.bin'                   ,'186';
-            'WrConfigInfo','DCOR_fml_5_Info_ConfigInfo_Ver_*.bin'                   ,'187';
-            'WrConfigInfo','DCOR_fml_6_Info_ConfigInfo_Ver_*.bin'                   ,'188';
-            'WrConfigInfo','DCOR_fml_7_Info_ConfigInfo_Ver_*.bin'                   ,'189';
-            'WrConfigInfo','DCOR_fml_8_Info_ConfigInfo_Ver_*.bin'                   ,'18A';
-            };
+            filenamesTableIndexLUT(end+1,:)={'WrConfigData','Algo_Dynamic_Configuration_VGA30_1_ConfigData_Ver_*.txt','133'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigData','Algo_Dynamic_Configuration_VGA30_2_ConfigData_Ver_*.txt','134'};
+%             filenamesTableIndexLUT(end+1,:)={'WrConfigData','Algo_Dynamic_Configuration_VGA30_3_ConfigData_Ver_*.txt','135'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_cml_0_Info_ConfigInfo_Ver_*.bin'                   ,'180'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_cml_1_Info_ConfigInfo_Ver_*.bin'                   ,'181'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_cml_2_Info_ConfigInfo_Ver_*.bin'                   ,'18B'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_0_Info_ConfigInfo_Ver_*.bin'                   ,'182'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_1_Info_ConfigInfo_Ver_*.bin'                   ,'183'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_2_Info_ConfigInfo_Ver_*.bin'                   ,'184'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_3_Info_ConfigInfo_Ver_*.bin'                   ,'185'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_4_Info_ConfigInfo_Ver_*.bin'                   ,'186'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_5_Info_ConfigInfo_Ver_*.bin'                   ,'187'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_6_Info_ConfigInfo_Ver_*.bin'                   ,'188'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_7_Info_ConfigInfo_Ver_*.bin'                   ,'189'};
+            filenamesTableIndexLUT(end+1,:)={'WrConfigInfo','DCOR_fml_8_Info_ConfigInfo_Ver_*.bin'                   ,'18A'};
+
     end
     for i=1:size(filenamesTableIndexLUT,1)
         fn = dir(fullfile(basedir,filenamesTableIndexLUT{i,2}));
-       
+        
         if(length(fn)~=1)
             warning('could not burn table %s',filenamesTableIndexLUT{i,2})
             continue;
