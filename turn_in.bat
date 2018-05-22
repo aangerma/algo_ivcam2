c95887cd543b17345245f3115038e511f790db1a
yresnick <yoni.resnick@intel.com>
2018-05-22 15:54:00 +0300

    avv: turn in parameter

--- turn_in.bat
+++ turn_in.bat
@@ -1,3 +1,3 @@
 :: run script from hosting directory
-python ..\algo_automation\tools\turn_in.py --repo-path ..\algo_ivcam2 --on-to-branch master --jenkins-job algo_ivcam2_turn_in
+python ..\algo_automation\tools\turn_in.py --repo-path ..\algo_ivcam2 --on-to-branch master --jenkins-job algo_ivcam2_turn_in %1
 
