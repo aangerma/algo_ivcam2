4f3e4115f027693477fea980cf9069edfe1630f7
vsurazh <vitaly.surazhsky@intel.com>
2018-06-26 12:21:09 +0300

    matlab calib fixes

--- scripts/calibScripts/matlabCameraCalib/cameraCalib.m
+++ scripts/calibScripts/matlabCameraCalib/cameraCalib.m
@@ -1,8 +1,8 @@
-%IRs = {};
+%IRs = {}; i = 1;
 %IRs{i} = hw.getFrame(30); figure; imagesc(IRs{i}.i); i = i + 1;
 % K=reshape([typecast(hw.read('CBUFspare'),'single');1],3,3)';
 
-load('checkerboard_ivcam2.mat');
+load('checkerboard_0626.mat');
 
 boardSize = [7 7];
 nImages = length(IRs);
@@ -11,14 +11,16 @@ imagePoints = zeros(boardSize(1)*boardSize(2), 2, nImages);
 
 %% detect checker board
 for i=1:nImages
-    ir = fliplr(IRs{i}.i);
+    %ir = fliplr(IRs{i}.i);
+    ir = (IRs{i}.i);
+    figure(77); imagesc(ir); hold on;
     [gridPoints, gridSize]=findCheckerboard(ir, boardSize);
     if (~isequal(gridSize, boardSize))
         warning(sprintf('Grid not found in image %g', i));
     end
     imagePoints(:,:,i) = gridPoints;
     
-    figure(77); imagesc(ir); hold on; plot(gridPoints(:,1), gridPoints(:,2), '+r'); hold off;
+    plot(gridPoints(:,1), gridPoints(:,2), '+r'); hold off;
 end
 
 %% compute world coordinates
@@ -30,15 +32,15 @@ imageSize = [size(IRs{i}.i, 1), size(IRs{i}.i, 2)];
 [params, ~, estimationErrors] = estimateCameraParameters(imagePoints, worldPoints, ...
     'ImageSize', imageSize);
 
-showReprojectionErrors(params);
-showExtrinsics(params);
+figure; showReprojectionErrors(params);
+figure; showExtrinsics(params);
 
 %% estimate errors
 errors = zeros(nImages,4);
 for i=1:nImages
     gridPoints = imagePoints(:,:,i);
-    vP = toVertices(gridPoints, fliplr(IRs{i}.z), K);
-    vM = toVertices(gridPoints, fliplr(IRs{i}.z), params.IntrinsicMatrix');
+    vP = toVertices(gridPoints, (IRs{i}.z), K);
+    vM = toVertices(gridPoints, (IRs{i}.z), params.IntrinsicMatrix');
     [errors(i,1), errors(i,2)] = gridError(vP, squareSize);
     [errors(i,3), errors(i,4)] = gridError(vM, squareSize);
 end
@@ -47,22 +49,24 @@ figure; plot(errors); legend('L1 K', 'L2 K', 'L1 Matlab', 'L2 Matlab');
 
 %% K computation
 
-xfov = 72;
-yfov = 52;
-xres = 640;
-yres = 480;
-marginL = 0;
-marginT = 0;
-xoffset = 0;
-yoffset = 0;
+xfov = 67.7611;
+yfov = 54.4618;
+xres = 682;
+yres = 519;
+
+
+marginL = 5;
+marginR = 37;
+marginT = 20;
+marginB = 19;
 
 p2axa = ( tand(xfov/2)* 2    / single(xres-1));
-p2axb = (-tand(xfov/2)*(1 - 2*single(marginL) / single(xres) + single(xoffset)));
+p2axb = (-tand(xfov/2)*(1 - 2*single(marginL) / single(xres) ));
 p2aya = ( tand(yfov/2)*2    / single(yres-1));
-p2ayb = (-tand(yfov/2)*(1 - 2*single(marginT) / single(yres) + single(yoffset)));
-
-K=[p2axa 0 p2axb;
+p2ayb = (-tand(yfov/2)*(1 - 2*single(marginT) / single(yres) ));
+[p2axa,p2axb,p2aya,p2ayb] = rangeCalc(regs);
+K33=[p2axa 0 p2axb;
    0 p2aya p2ayb;
    0 0 1];
 
-Kinv=pinv(K);
+K33inv=pinv(K33);
