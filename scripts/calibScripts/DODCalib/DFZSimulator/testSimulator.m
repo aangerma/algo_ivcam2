1ac0282e7f46172d18c8f80932221317c2ee415d
tmund <tal.mund@intel.com>
2018-08-02 19:55:55 +0300

    Comparing Zenith in X  vs angX dsm offset with and without baseline

--- scripts/calibScripts/DODCalib/DFZSimulator/testSimulator.m
+++ scripts/calibScripts/DODCalib/DFZSimulator/testSimulator.m
@@ -62,3 +62,33 @@ for zenith = -15:15
     i = i+1;
 end
 plot(zenith,minerr);
+
+%% Next question - what happens in our case where the baseline has an angle of 2 degrees? What happens to the DFZ calibration?
+clear
+fw = Pipe.loadFirmware('C:\source\algo_ivcam2\+Calibration\initScript');
+% baseline = single(31);
+% fovx = single(65);
+% fovy = single(55);
+% txDelay = single(5050);
+% regs.DEST.baseline = baseline;
+% regs.FRMW.xfov = fovx;
+% regs.FRMW.yfov = fovy;
+% regs.DEST.txFRQpd = [txDelay, txDelay, txDelay];
+% fw.setRegs(regs,'');
+[regs,luts] = fw.get();
+
+CBParams.size = 30;
+CBParams.bsz = [9,13];
+dist = 500;
+[rpt,cbxyz] = simulateCB(dist,regs,CBParams);
+[o,minerr]=calibDFZFromRPT(rpt,regs,1,0);
+
+
+fw = Pipe.loadFirmware('C:\source\algo_ivcam2\+Calibration\initScript');
+% baseline = single(31);
+% regs2.DEST.baseline = baseline;
+% fw.setRegs(regs2,'');
+% [regs,luts] = fw.get();
+
+rptShift = rpt; rptShift(:,:,2) = rptShift(:,:,2) - 2/(regs.FRMW.xfov/4)*2047;
+[~,minerr]=calibDFZFromRPT(rptShift,regs,1,0);
