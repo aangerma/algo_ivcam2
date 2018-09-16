8893bf967ce489e859327637f2a65617c454e0fe
vsurazh <vitaly.surazhsky@intel.com>
2018-09-16 17:40:50 +0300

    all fits done

--- +Validation/+aux/mos.m
+++ +Validation/+aux/mos.m
@@ -1,5 +1,6 @@
-function [res] = mos(ir, expectedGridSize, verbose)
-ir = double(ir);
+function [res] = mos(irImg, zImg, expectedGridSize, verbose)
+irImg = double(irImg);
+zImg = double(zImg);
 
 if(~exist('expectedGridSize','var'))
     expectedGridSize=[];
@@ -9,7 +10,7 @@ if(~exist('verbose','var'))
 end
 
 %% fine grid
-[pt, gridSize] = Validation.aux.findCheckerboard(ir, expectedGridSize);
+[pt, gridSize] = Validation.aux.findCheckerboard(irImg, expectedGridSize);
 
 if (any(gridSize < 2))
     if verbose
@@ -34,7 +35,7 @@ centers = (pts(1:end-1,1:end-1,:) + pts(1:end-1,2:end,:) +...
     pts(2:end,1:end-1,:) + pts(2:end,2:end,:))/4;
 
 if (verbose)
-    figure; imagesc(ir);
+    figure; imagesc(irImg);
     hold on; plot(pts(:,:,1), pts(:,:,2), '+r');
     hold on; plot(centers(:,:,1), centers(:,:,2), '+k');
 end
@@ -43,11 +44,14 @@ end
 % make sure the too-right cell is white
 horiz = false;
 % if cell(1,1) is black
-if (ir(round(centers(1,1,2)),round(centers(1,1,1))) < ir(round(centers(1,2,2)),round(centers(1,2,1))))
+if (irImg(round(centers(1,1,2)),round(centers(1,1,1))) < irImg(round(centers(1,2,2)),round(centers(1,2,1))))
     horiz = true;
 end
 
 fCrop = 0.45;
+
+barW = zeros(size(pts,1)-1, (size(pts,2)-1)/2);
+barH = zeros(size(pts,1)-1, (size(pts,2)-1)/2);
 
 %% cell iteratons
 for j=1:size(pts,1)-1
@@ -56,8 +60,10 @@ for j=1:size(pts,1)-1
         R = squeeze([pts(j,i,:) pts(j,i+1,:) pts(j+1,i,:) pts(j+1,i+1,:)]);
         c = sum(R,1)/4;
         R = R*(1-fCrop) + c*fCrop;
-        plot(c(1), c(2), 'Or');
-        patch(R(:,1), R(:,2), 'r');
+        if (verbose)
+            plot(c(1), c(2), 'Or');
+            patch(R(:,1), R(:,2), 'r');
+        end
         
         [x0 i0] = max([R(1,1) R(3,1)]);
         i0Min = iff(i0 == 1, 3, 1);
@@ -73,7 +79,10 @@ for j=1:size(pts,1)-1
         
         p0 = (RR(1,:) + RR(3,:))/2;
         p1 = (RR(2,:) + RR(4,:))/2;
-        height = min(RR(3,2) - RR(1,2), RR(4,2) - RR(2,2));
+        tunnelHeight = ceil(min(RR(3,2) - RR(1,2), RR(4,2) - RR(2,2)));
+        
+        k = (ib + 1)/2;
+        [barW(j,k), barH(j,k)] = analyzeHorizontalMos(zImg, p0, p1, tunnelHeight);
         
         %[hTrans(i,j),hCont(i,j)] = analyzeHorizontalMos(ir, p0, p1, tunnelWidth);
     end
@@ -85,7 +94,7 @@ xPts(1,:) = reshape(pt(:,1), gridSize);
 res.gridSize = gridSize;
 res.points = pt;
 
-figure; imagesc(ir); hold on; plot(pt(:,1), pt(:,2), '+r');
+figure; imagesc(irImg); hold on; plot(pt(:,1), pt(:,2), '+r');
 
 pts=reshape(pt(:,1)+1j*pt(:,2),gridSize);
 
@@ -108,7 +117,7 @@ for j=1:hSize(2)
     for i=1:hSize(1)
         p0 = pts(i, j);
         p1 = pts(i, j+1);
-        [hTrans(i,j),hCont(i,j)] = analyzeHorizontalEdge(ir, p0, p1, tunnelWidth);
+        [hTrans(i,j),hCont(i,j)] = analyzeHorizontalEdge(irImg, p0, p1, tunnelWidth);
     end
 end
 
@@ -130,12 +139,14 @@ res.contStd = std(cont(:));
 
 end
     
-function [width,contrast] = analyzeHorizontalMos(zImg, p0, p1, tunnelWidth)
+function [width,height] = analyzeHorizontalMos(zImg, p0, p1, tunnelWidth)
+subSamples = 4;
+
 x0 = p0(1); x1 = p1(1);
 y0 = p0(2); y1 = p1(2);
 
 hLine = polyfit([x0 x1], [y0 y1], 1);
-    
+
 xRange = ceil(x0):floor(x1);
 yCenters = polyval(hLine, xRange);
 yCenter = mean([yCenters(1) yCenters(end)]);
@@ -143,7 +154,7 @@ yRangeLen = tunnelWidth + ceil(abs(yCenters(1)-yCenters(end))) + 2;
 yRange0 = floor(yCenter - yRangeLen/2);
 yRange1 = yRange0 + yRangeLen;
 yRange = yRange0:yRange1;
-yInterpRange = -tunnelWidth/2:0.25:tunnelWidth/2;
+yInterpRange = -tunnelWidth/2:(1/subSamples):tunnelWidth/2;
 zInterp = zeros(length(yInterpRange), length(xRange));
 for ix=1:length(xRange)
     x = xRange(ix);
@@ -161,15 +172,19 @@ zf = Y(:)*p(1)+X(:)*p(2)+p(3);
 %figure; mesh(zInterp);
 %hold on; mesh(reshape(zf, size(zInterp)));
 
-zInterp = zInterp - reshape(zf, size(zInterp));
+zFixed = zInterp - reshape(zf, size(zInterp));
 %figure; imagesc(zInterp);
 
-zCurve = mean(zInterp,2);
+zCurve = mean(zFixed,2);
 zIntegral = cumsum(zCurve);
 
+zSubMM = 8;
+
 [fitCurve,~,~, rise, sigm_params] = fitting.riseFitting(zIntegral);
-width = rise / 4;
-contrast = abs(fitCurve(end)-fitCurve(1));
+width = rise / subSamples;
+pxPeak = round(sigm_params(3));
+height = abs(median(zFixed(pxPeak-1:pxPeak+1)))/zSubMM;
+
 %figure; plot([zIntegral fitCurve]);
 
 end
