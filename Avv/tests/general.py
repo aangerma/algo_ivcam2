dfdeeffe2326c8b96dc9e3e9d5384dc638460d88
yresnick <yoni.resnick@intel.com>
2018-11-05 15:24:00 +0200

    Add reed multipole bin files to matlab io and reset for robot

---
+++ Avv/tests/general.py
@@ -0,0 +1,13 @@
+#! python3
+import slash
+import sys
+sys.path.insert(0, r"Avv\tests")
+# import a_common
+sys.path.insert(0, r"..\algo_automation\infra")
+import robot
+
+
+@slash.tag('robot')
+def test_reset_robot():
+    robot.reset()
+
