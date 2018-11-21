8a74b46c4f07f26cc15d6646be95a11ff3f2280f
tmund <tal.mund@intel.com>
2018-11-21 14:12:34 +0200

    tmp

---
+++ scripts/maxRange/multiFocal/testMF.m
@@ -0,0 +1,23 @@
+
+hw = HWinterface();
+hw.startStream();
+
+r=Calibration.RegState(hw);
+r.add('invbypass'    ,true     );
+r.add('sphericalEn'    ,true     );
+r.set();
+hw.runScript('MultiFocalCfg.txt');
+hw.runScript('MultiFocalROI.txt');
+
+for i = 1:50
+%    hw.cmd(cmd{i}); 
+   frame(i) = hw.getFrame();
+end
+z = zeros(50,480,640);
+for i = 1:50
+   z(i,:,:) = frame(i).z/8;
+end
+figure, imagesc(squeeze(std(z,[],1)));
+
+
+r.reset();
