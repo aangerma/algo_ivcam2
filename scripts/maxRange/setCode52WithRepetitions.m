a45d40cbaa2cc953b38a61c3233f483ef800ddc2
tmund <tal.mund@intel.com>
2018-11-26 16:29:28 +0200

    Start working on repeat

---
+++ scripts/maxRange/setCode52WithRepetitions.m
@@ -0,0 +1,14 @@
+function [  ] = setCode52WithRepetitions( hw, repeat)
+%SETCODE26WITHREPETITIONS loads code 52 and do repetitions for each symbol.
+% Trim the code at length 52.
+
+
+% FRMWtxCode_000 h69966665
+% FRMWtxCode_001 h000A6AA9
+% GNRLcodeLength d52
+code52 = zeros(
+code52 = flipud(Codes.propCode(52,1));
+code52 = binaryVectorToHex(reshape(fliplr(Codes.propCode(52,1)'));
+
+end
+
