ca840eb7561b6482574d49226da998232241d475
tmund <tal.mund@intel.com>
2018-11-26 22:44:57 +0200

    Repeat symbols without change to tx rate and rx rate

--- scripts/maxRange/setCode52WithRepetitions.m
+++ scripts/maxRange/setCode52WithRepetitions.m
@@ -2,13 +2,25 @@ function [  ] = setCode52WithRepetitions( hw, repeat)
 %SETCODE26WITHREPETITIONS loads code 52 and do repetitions for each symbol.
 % Trim the code at length 52.
 
-
 % FRMWtxCode_000 h69966665
 % FRMWtxCode_001 h000A6AA9
 % GNRLcodeLength d52
-code52 = zeros(
-code52 = flipud(Codes.propCode(52,1));
-code52 = binaryVectorToHex(reshape(fliplr(Codes.propCode(52,1)'));
+origLength = 52;
+baseLen = floor(origLength/repeat);
+code52 = Codes.propCode(origLength,1)';
+baseCode = code52(1:baseLen);
+repeatedCode = reshape((repmat(baseCode,repeat,1)),[],1)';
+padding = ((-1).^(1:origLength-length(repeatedCode))+1)/2;
+repeatedCode = [repeatedCode,padding];
 
+txcode = zeros(1,64);
+txcode(1:52) = repeatedCode;
+txcode = binaryVectorToHex(fliplr(reshape(txcode',32,2)'));
+
+txregs.FRMW.txCode = uint32([hex2dec(txcode{1}),hex2dec(txcode{2}),0,0]);
+txregs.GNRL.codeLength = uint8(origLength);
+txregs.FRMW.coarseSampleRate = uint8(4);
+
+hw.setCode(txregs,0);
 end
 
