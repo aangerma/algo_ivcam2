69f7fc250828a6558a89e935e10bc4adf8ac20a6
tmund <tal.mund@intel.com>
2018-12-04 09:42:00 +0200

    Capturing sscript

--- scripts/maxRange/collectMaxRangeFrames.m
+++ scripts/maxRange/collectMaxRangeFrames.m
@@ -1,107 +1,144 @@
-function collectMaxRangeFrames(hw)
+function collectMaxRangeFrames()
 % Once the camera is positioned targeting the 10% target this script will
 % collect 100 frames for each configuration.
 % It will save the frames sturct array in a mat file in a destination at X
 % drive.
 
-% Configurations:
+% Configurations (for 64 and 52 code):
 % 1. Default.
-% 2. Half vertical resolution plus upscale.
-% 3. Tx rate of 0.5/0.25 GHz using multi focal.
-% 4. Tx rate of 0.5/0.25 GHz using multi focal with LPF disabled.
-% 5. Tx rate of 0.5/0.25 using repetitions in the actual code.
-% 6. Tx rate of 0.5/0.25 using repetitions in the actual code with LPF disabled.
+% 2. Half horizontal resolution plus upscale X.
+% 3. Half vertical resolution plus upscale Y.
+% 4. Tx rate of 0.5/0.25 GHz using multi focal.
+% 5. Tx rate of 0.5/0.25 GHz using multi focal with LPF disabled.
+% 6. Tx rate of 0.5/0.25 using repetitions in the actual code.
+% 7. Tx rate of 0.5/0.25 using repetitions in the actual code with LPF disabled.
 
-hw.getFrame(60); % Activate and let it warm up for a minute.
-hw.setReg('sphericalEn',1);% Since half res is in spherical, we want the mall to use it.
-hw.shadowUpdate();
-savedir = 'X:\Data\IvCam2\maxRange\testRecords';
+savedir = 'X:\Data\IvCam2\maxRange\testRecordsLatest';
 N = 100;
-% default
-fn = 'default';
-fn = fullfile(savedir,[fn,'.mat']);
+hw = HWinterface;
+hw.getFrame(10); % Activate and let it warm up 
+hw.setReg('sphericalEn',1);% Since half res is in spherical, we want them all to use it.
+hw.shadowUpdate;
+pause(0.1);
+frameI = hw.getFrame(30).i;
+fn = fullfile(savedir,['IR','.mat']);
+save(fn,'frameI');
 
-frames = collectFrames(hw,N);
-save(fn,'frames');
+for codeLen = [52,64]
+    % default
+    hw = openHWInterface(codeLen);
+    fn = ['default','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
 
-% tx_rate_500M_MF_LPF_enabled
-fn = 'tx_rate_500M_MF_LPF_enabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 2; LPFEn = 1; setTxRateMF( hw, factor, LPFEn );
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_500M_MF_LPF_disabled
-fn = 'tx_rate_500M_MF_LPF_disabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 2; LPFEn = 0; setTxRateMF( hw, factor, LPFEn );
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_250M_MF_LPF_enabled
-fn = 'tx_rate_250M_MF_LPF_enabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 4; LPFEn = 1; setTxRateMF( hw, factor, LPFEn );
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_250M_MF_LPF_disabled
-fn = 'tx_rate_250M_MF_LPF_disabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 4; LPFEn = 0; setTxRateMF( hw, factor, LPFEn );
-frames = collectFrames(hw,N);
-save(fn,'frames');
+    % tx_rate_500M_MF_LPF_enabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_500M_MF_LPF_enabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 2; LPFEn = 1; setTxRateMF( hw, factor, LPFEn );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_500M_MF_LPF_disabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_500M_MF_LPF_disabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 2; LPFEn = 0; setTxRateMF( hw, factor, LPFEn );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_250M_MF_LPF_enabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_250M_MF_LPF_enabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 4; LPFEn = 1; setTxRateMF( hw, factor, LPFEn );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_250M_MF_LPF_disabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_250M_MF_LPF_disabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 4; LPFEn = 0; setTxRateMF( hw, factor, LPFEn );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
 
-disableMF(hw);
+    % tx_rate_500M_LPF_enabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_500M_LPF_enabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 2; LPFEn = 1; 
+    AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
+    hw.cmd(AFE_LPF_Cmd);
+    setCodeWithRepetitions( hw,codeLen, factor);
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_500M_LPF_disabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_500M_LPF_disabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 2; LPFEn = 0; 
+    AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
+    hw.cmd(AFE_LPF_Cmd);
+    setCodeWithRepetitions( hw,codeLen, factor);
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_250M_LPF_enabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_250M_LPF_enabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 4; LPFEn = 1; 
+    AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
+    hw.cmd(AFE_LPF_Cmd);
+    setCodeWithRepetitions( hw,codeLen, factor);
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
+    % tx_rate_250M_LPF_disabled
+    hw = openHWInterface(codeLen);
+    fn = ['tx_rate_250M_LPF_disabled','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    factor = 4; LPFEn = 0; 
+    AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
+    hw.cmd(AFE_LPF_Cmd);
+    setCodeWithRepetitions( hw,codeLen, factor);
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
 
-% tx_rate_500M_LPF_enabled
-fn = 'tx_rate_500M_LPF_enabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 2; LPFEn = 1; 
-AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
-hw.cmd(AFE_LPF_Cmd);
-setCode52WithRepetitions( hw, factor);
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_500M_LPF_disabled
-fn = 'tx_rate_500M_LPF_disabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 2; LPFEn = 0; 
-AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
-hw.cmd(AFE_LPF_Cmd);
-setCode52WithRepetitions( hw, factor);
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_250M_LPF_enabled
-fn = 'tx_rate_250M_LPF_enabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 4; LPFEn = 1; 
-AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
-hw.cmd(AFE_LPF_Cmd);
-setCode52WithRepetitions( hw, factor);
-frames = collectFrames(hw,N);
-save(fn,'frames');
-% tx_rate_250M_LPF_disabled
-fn = 'tx_rate_250M_LPF_disabled';
-fn = fullfile(savedir,[fn,'.mat']);
-factor = 4; LPFEn = 0; 
-AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
-hw.cmd(AFE_LPF_Cmd);
-setCode52WithRepetitions( hw, factor);
-frames = collectFrames(hw,N);
-save(fn,'frames');
+    % halfResX
+    hw = openHWInterface(codeLen);
+    fn = ['halfResX','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    setHalfRes( hw,1 );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
 
-% Restor default code
-factor = 1; LPFEn = 1; 
-AFE_LPF_Cmd = strcat('mwd a003006c a0030070',[' ',dec2hex(LPFEn)],' // Enable LPF in the AFE');
-hw.cmd(AFE_LPF_Cmd);
-setCode52WithRepetitions( hw, factor);
+    % halfResY
+    hw = openHWInterface(codeLen);
+    fn = ['halfResX','_',num2str(codeLen)];
+    fn = fullfile(savedir,[fn,'.mat']);
+    setHalfRes( hw,1 );
+    frames = collectFrames(hw,N);
+    save(fn,'frames');
+    clear hw
 
-% halfRes
-fn = 'halfRes';
-fn = fullfile(savedir,[fn,'.mat']);
-setHalfRes( hw );
-frames = collectFrames(hw,N);
-save(fn,'frames');
 end
+end
+function hw = openHWInterface(codeLen)
+hw = HWinterface;
+hw.getFrame(10); % Activate and let it warm up 
+hw.setReg('sphericalEn',1);% Since half res is in spherical, we want them all to use it.
+hw.setReg('DESTaltIrEn',1);
+hw.setCode(Codes.codeRegs(codeLen,4),0);
 
+end
 function frames = collectFrames(hw,N)
     pause(0.30);
     for i = 1:N
--- scripts/maxRange/setCodeWithRepetitions.m
+++ scripts/maxRange/setCodeWithRepetitions.m
@@ -1,20 +1,20 @@
-function [  ] = setCode52WithRepetitions( hw, repeat)
-%SETCODE26WITHREPETITIONS loads code 52 and do repetitions for each symbol.
+function [  ] = setCodeWithRepetitions( hw,origLength,repeat)
+%SETCODE26WITHREPETITIONS loads code 52 or 64 and do repetitions for each symbol.
 % Trim the code at length 52.
 
 % FRMWtxCode_000 h69966665
 % FRMWtxCode_001 h000A6AA9
 % GNRLcodeLength d52
-origLength = 52;
+assert(any(origLength == [64,52]));
 baseLen = floor(origLength/repeat);
-code52 = Codes.propCode(origLength,1)';
-baseCode = code52(1:baseLen);
+codeO = Codes.propCode(origLength,1)';
+baseCode = codeO(1:baseLen);
 repeatedCode = reshape((repmat(baseCode,repeat,1)),[],1)';
 padding = ((-1).^(1:origLength-length(repeatedCode))+1)/2;
 repeatedCode = [repeatedCode,padding];
 
 txcode = zeros(1,64);
-txcode(1:52) = repeatedCode;
+txcode(1:origLength) = repeatedCode;
 txcode = binaryVectorToHex(fliplr(reshape(txcode',32,2)'));
 
 txregs.FRMW.txCode = uint32([hex2dec(txcode{1}),hex2dec(txcode{2}),0,0]);
