34e271c9be8bc1ce57b8530b4324027b7c067ce6
tmund <tal.mund@intel.com>
2018-12-27 17:35:06 +0200

    negative baseline

--- scripts/calibScripts/DODCalib/DFZSimulator/calibDFZ.m
+++ scripts/calibScripts/DODCalib/DFZSimulator/calibDFZ.m
@@ -1,4 +1,4 @@
-function [outregs,minerr,eFit,darrNew]=calibDFZ(darr,regs,calibParams,fprintff,verbose,iseval,x0)
+function [outregs,minerr,eFit,darrNew]=calibDFZ(darr,regs,calibParams,fprintff,verbose,iseval,x0,flipBaseline)
     % When eval == 1: Do not optimize, just evaluate. When it is not there,
     % train.
     par = calibParams.dfz;
@@ -15,59 +15,19 @@ function [outregs,minerr,eFit,darrNew]=calibDFZ(darr,regs,calibParams,fprintff,v
     if(~exist('fprintff','var')|| isempty(fprintff))
         fprintff=@(varargin) fprintf(varargin{:});
     end
-    if(~exist('x0','var'))% If x0 is not given, using the regs used i nthe recording
+    if(isempty(x0))% If x0 is not given, using the regs used i nthe recording
         x0 = double([regs.FRMW.xfov regs.FRMW.yfov regs.DEST.txFRQpd(1) regs.FRMW.laserangleH regs.FRMW.laserangleV regs.FRMW.projectionYshear 0 0]);
     end
     
     
-    for i = 1:numel(darr)
-        % Get r from d.z
-        if ~regs.DEST.depthAsRange
-            [~,r] = Pipe.z16toVerts(darr(i).z,regs);
-        else
-            r = double(darr(i).z)/bitshift(1,regs.GNRL.zMaxSubMMExp);
-        end
-        % get rtd from r
-        [~,~,~,~,~,~,sing]=Pipe.DEST.getTrigo(size(r),regs);
-        C=2*r*regs.DEST.baseline.*sing- regs.DEST.baseline2;
-        rtd=r+sqrt(r.^2-C);
-        rtd=rtd+regs.DEST.txFRQpd(1);
-        
-        %calc angles per pixel
-        [yg,xg]=ndgrid(0:size(rtd,1)-1,0:size(rtd,2)-1);
-        if(regs.DIGG.sphericalEn)
-            yy = double(yg);
-            xx = double((xg)*4);
-            xx = xx-double(regs.DIGG.sphericalOffset(1));
-            yy = yy-double(regs.DIGG.sphericalOffset(2));
-            xx = xx*2^10;%bitshift(xx,+12-2);
-            yy = yy*2^12;%bitshift(yy,+12);
-            xx = xx/double(regs.DIGG.sphericalScale(1));
-            yy = yy/double(regs.DIGG.sphericalScale(2));
-            
-            angx = single(xx);
-            angy = single(yy);
-        else
-            [angx,angy]=Calibration.aux.xy2angSF(xg,yg,regs,0);
-        end
-        
-        %find CB points
-        warning('off','vision:calibrate:boardShouldBeAsymmetric') % Supress checkerboard warning
-        [p,bsz] = Calibration.aux.CBTools.findCheckerboard(normByMax(double(darr(i).i)), [9,13]); % p - 3 checkerboard points. bsz - checkerboard dimensions.
-        if isempty(p)
-            fprintff('Error: checkerboard not detected!');
-        end
-        it = @(k) interp2(xg,yg,k,reshape(p(:,1)-1,bsz),reshape(p(:,2)-1,bsz)); % Used to get depth and ir values at checkerboard locations.
-        
-        %rtd,phi,theta
-        darr(i).rpt=cat(3,it(rtd),it(angx),it(angy)); % Convert coordinate system to angles instead of xy. Makes it easier to apply zenith optimization.
-        
-    end
-    
     %%
+    if flipBaseline
+       regs.DEST.baseline = - regs.DEST.baseline;
+    end
     xL = [par.fovxRange(1) par.fovyRange(1) par.delayRange(1) par.zenithxRange(1) par.zenithyRange(1) par.projectionYshear(1) par.dsmXOffset(1) par.dsmYOffset(1)];
     xH = [par.fovxRange(2) par.fovyRange(2) par.delayRange(2) par.zenithxRange(2) par.zenithyRange(2) par.projectionYshear(2) par.dsmXOffset(2) par.dsmYOffset(2)];
     regs = x2regs(x0,regs);
+    
     if iseval
         [minerr,eFit]=errFunc(darr,regs,x0,FE);
         outregs = [];
--- scripts/calibScripts/DODCalib/DFZSimulator/simulateCB.m
+++ scripts/calibScripts/DODCalib/DFZSimulator/simulateCB.m
@@ -1,4 +1,4 @@
-function [rpt,cbxyz] = simulateCB(dist,regs,CBParams,fe)
+function [rpt,cbxyz] = simulateCB(dist,yRot,regs,CBParams,noiseStdAng,noiseStdR)
 % SimulateSB receives:
 % 1. Distance from board in mm
 % 2. The DFZ parameters
@@ -17,20 +17,26 @@ end
 
 [cbx,cby] = meshgrid(linspace(-(CBParams.bsz(2)-1)/2*CBParams.size,(CBParams.bsz(2)-1)/2*CBParams.size,CBParams.bsz(2)),...
                      linspace(-(CBParams.bsz(1)-1)/2*CBParams.size,(CBParams.bsz(1)-1)/2*CBParams.size,CBParams.bsz(1)));
-cbz = ones(size(cbx))*dist;
-cbxyz = cat(3,cbx,cby,cbz);
+cbxz = [reshape(cbx,[],1),zeros(prod(CBParams.bsz),1)];
+rotmat = [cosd(yRot),sind(yRot);-sind(yRot),cosd(yRot)];
+cbxz = cbxz*rotmat;
+cbxyz = cat(3,reshape(cbxz(:,1),CBParams.bsz),cby,reshape(cbxz(:,2),CBParams.bsz));
+cbxyz(:,:,3) = cbxyz(:,:,3)+dist;
 
+% cbxyz = cbxyz + randn(size(cbxyz))*noiseStd;
+
+figure,plot3(cbxyz(:,:,1),cbxyz(:,:,2),cbxyz(:,:,3),'r*')
 % Calculate the range for each point (include the delay)
 cbr = sqrt(sum(cbxyz.^2,3));
 % get rtd from r
-sing = cbxyz(:,:,1)./sqrt(cbxyz(:,:,1).^2+cbxyz(:,:,2).^2+cbxyz(:,:,3).^2);
+sing = cbxyz(:,:,1)./cbr;
 C=2*cbr*regs.DEST.baseline.*sing - regs.DEST.baseline2;
 rtd=cbr+sqrt(cbr.^2-C);
 rtd=rtd+regs.DEST.txFRQpd(1);
 % Calculate angx and angy
 vec = reshape(cbxyz,[],3);
-vec = applyExpander(vec,fliplr(fe));
+% vec = applyExpander(vec,fliplr(fe));
 [angx,angy] = vec2ang(vec,regs);
-rpt = cat(3,rtd,reshape(angx,size(rtd)),reshape(angy,size(rtd)));
+rpt = cat(3,rtd+ randn(size(rtd))*noiseStdR,reshape(angx,size(rtd))+ randn(size(rtd))*noiseStdAng,reshape(angy,size(rtd))+ randn(size(rtd))*noiseStdAng);
 end
 
