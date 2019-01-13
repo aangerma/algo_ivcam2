c49968fc038f6c5a2de5d70063da7fa75e27b8d2
heliyahu <hila.eliyahu@intel.com>
2019-01-13 15:35:19 +0200

    Update fwBootCalcs.m
    ang2XY boot calc description

--- +Pipe/+DIGG/+FRMW/fwBootCalcs.m
+++ +Pipe/+DIGG/+FRMW/fwBootCalcs.m
@@ -1,6 +1,21 @@
 function [regs,autogenRegs,autogenLuts] = fwBootCalcs(regs,luts,autogenRegs,autogenLuts)
 
 %% =======================================DIGG - ang2xy- calib res =======================================
+% Ang2xy is the transformation from angular data to rasterized grid over the projected plane.
+
+%  The pre calculations produces:
+
+%  *18 floating point coefficients for the runtime calculations (DIGG: nx,dx,ny,dy.)
+
+%  *4 output registers for other blocks in the pipe: DIGG.angXfactor, DIGG.angYfactor.
+
+%  2 parameters for firmware to save: FRMW.xres,FRMW.yres.
+
+% Ang2xyCoeff function should be calculated when one of the following is change: 
+
+% Regs from EPROM: regs.FRMW.xfov, regs.FRMW.yfov, regs.FRMW.laserangleH,regs.FRMW.laserangleV, regs.GNRL.imgHsize,regs.GNRL.imgVsize,regs.FRMW.marginL/R/T/B, regs.FRMW.guardBandH,regs.FRMW.guardBandV, regs.FRMW.xR2L,regs.FRMW.xoffset, regs.FRMW.yoffset
+
+%  Regs from external configuration: regs.GNRL.rangeFinder,regs.FRMW.mirrorMovmentMode, regs.FRMW.marginL/R/T/B, regs.FRMW.yflip,  
 [regs,autogenRegs] = ang2xyCoeff(regs,autogenRegs);
 
 
