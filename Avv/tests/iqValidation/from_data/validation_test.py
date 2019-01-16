c206a501a93feec5b956674a5af19a279861ccc1
yresnick <yoni.resnick@intel.com>
2019-01-16 13:03:46 +0200

    avv: iqValidation: support  robot range test

---
+++ Avv/tests/iqValidation/from_data/validation_test.py
@@ -0,0 +1,46 @@
+# ! python3
+import slash
+import sys
+
+sys.path.insert(0, r"..\algo_automation\tests")
+import runIqValidation
+
+
+def test_validation_regression():
+    filePath = r'Avv/tests/iqValidation/from_data/regression.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
+
+
+@slash.tag('turn_in')
+def test_validation_turn_in():
+    filePath = r'Avv/tests/iqValidation/from_data/turn_in.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
+
+
+def test_validation_es2():
+    filePath = r'Avv/tests/iqValidation/from_data/es2.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
+
+
+@slash.tag('turn_in')
+def test_validation_ivs():
+    filePath = r'Avv/tests/iqValidation/from_data/ivs.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
+
+
+@slash.tag('ds')
+def test_validation_ds5u_camera():
+    filePath = r'Avv/tests/iqValidation/from_data/ds5u_camera.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
+
+
+@slash.tag('ds')
+def test_validation_d4m_camera():
+    filePath = r'Avv/tests/iqValidation/from_data/d4m_camera.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
