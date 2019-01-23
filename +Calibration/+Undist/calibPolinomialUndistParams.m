7e2e3380448dc7a68fdc6cb63000ff641d6ce974
azabatan <aviad.zabatani@intel.com>
2019-01-23 12:06:01 +0200

    add new targets. support new polinomial distortion correction

--- +Calibration/+Undist/calibPolinomialUndistParams.m
+++ +Calibration/+Undist/calibPolinomialUndistParams.m
@@ -1,47 +1,52 @@
 function [ abest, meanerr] = calibPolinomialUndistParams( darr,regs,calibParams,aEval )
-opt.maxIter = 100000;
-opt.OutputFcn = [];
-opt.TolFun = 1e-8;
-opt.TolX = 1e-8;
-opt.Display ='none';
-func = @(x,a) x/2047*a(1)+(x/2047).^2*a(2)+(x/2047).^3*a(3);
-
-optFunc = @(a) (errFunc(darr,regs,a,func,0));
-if exist('aEval','var')
-    abest = aEval;
-    [meanerr,errors]=errFunc(darr,regs,abest,func,0);
-%     
-%     for i = 1:numel(darr)
-%        dUndist(i) = darr(i);
-%        dUndist(i).rpt(:,:,2) = darr(i).rpt(:,:,2)+func(darr(i).rpt(:,:,2),abest);
-%     end
+    opt.maxIter = 100000;
+    opt.OutputFcn = [];
+    opt.TolFun = 1e-8;
+    opt.TolX = 1e-8;
+    opt.Display ='none';
+    func = @(x,a) x/2047*a(1)+(x/2047).^2*a(2)+(x/2047).^3*a(3);
+    FE = [];
+    if calibParams.fovExpander.valid
+        FE = calibParams.fovExpander.table;
+    end
+    optFunc = @(a) (errFunc(darr,regs,a,func,FE,0));
+    if exist('aEval','var')
+        abest = aEval;
+        [meanerr,errors]=errFunc(darr,regs,abest,func,FE,0);
+        %
+        %     for i = 1:numel(darr)
+        %        dUndist(i) = darr(i);
+        %        dUndist(i).rpt(:,:,2) = darr(i).rpt(:,:,2)+func(darr(i).rpt(:,:,2),abest);
+        %     end
+        
+        return
+    end
+    a0 = double([0,0,0]);
+    [meanerr,errors]=errFunc(darr,regs,a0,func,FE,0);
+    abest = fminsearch(@(a) optFunc(a),a0,opt);
+    abest = fminsearch(@(a) optFunc(a),abest,opt);
+    [meanerr,errors]=errFunc(darr,regs,abest,func,FE,0);
+    %
+    % for i = 1:numel(darr)
+    %    dUndist(i) = darr(i);
+    %    dUndist(i).rpt(:,:,2) = darr(i).rpt(:,:,2)+func(darr(i).rpt(:,:,2),abest);
+    % end
     
-    return 
-end
-a0 = double([0,0,0]);
-[meanerr,errors]=errFunc(darr,regs,a0,func,0);
-abest = fminsearch(@(a) optFunc(a),a0,opt);
-abest = fminsearch(@(a) optFunc(a),abest,opt);
-[meanerr,errors]=errFunc(darr,regs,abest,func,0);
-% 
-% for i = 1:numel(darr)
-%    dUndist(i) = darr(i);
-%    dUndist(i).rpt(:,:,2) = darr(i).rpt(:,:,2)+func(darr(i).rpt(:,:,2),abest);
-% end
-
 end
 
 
-function [e,eAll]=errFunc(darr,regs,a,func,verbose)
+function [e,eAll]=errFunc(darr,regs,a,func,FE,verbose)
     if ~exist('verbose','var')
         verbose = 0;
     end
+    e = [];
+    eFit = [];
     for i = 1:numel(darr)
         d = darr(i);
-        vUnit = Calibration.aux.ang2vec(d.rpt(:,2)+func(d.rpt(:,2),a),d.rpt(:,3),regs)';
+        vUnit = Calibration.aux.ang2vec(d.rpt(:,2)+func(d.rpt(:,2),a),d.rpt(:,3),regs,FE)';
         % Update scale to take margins into acount.
-        sing = vUnit(:,:,1);
-        rtd_=d.rpt(:,:,1)-regs.DEST.txFRQpd(1);
+         sing = vUnit(:,1);
+        rtd_=d.rpt(:,1)-regs.DEST.txFRQpd(1);
         r = (0.5*(rtd_.^2 - regs.DEST.baseline2))./(rtd_ - regs.DEST.baseline.*sing);
         v = vUnit.*r;
         numVert = d.grid(1)*d.grid(2);
@@ -55,5 +60,5 @@ function [e,eAll]=errFunc(darr,regs,a,func,verbose)
         end
     end
     eAll = e;
-    e = mean(e);    
+    e = mean(e);
 end
