c0e8d61d6342c056be5058b54a7aa3799ba59d98
mayakip <maya.kiperwas.gal@intel.com>
2019-01-30 12:59:12 +0200

    Added new register and lut for teh FOV expander model and also a utility function 'feVec2Mat that arranges the lut to a matrix in the correct type

--- +Pipe/bootCalcs.m
+++ +Pipe/bootCalcs.m
@@ -1,6 +1,6 @@
 function [regs,autogenRegs,autogenLuts] = bootCalcs(regs,luts,autogenRegs,autogenLuts)
 
-[regs,autogenRegs] = generalRegisters(regs,autogenRegs);
+[regs,autogenRegs,autogenLuts] = generalRegisters(regs,autogenRegs,autogenLuts);
 [regs,autogenRegs,autogenLuts] = Pipe.DIGG.FRMW.bootCalcs(regs,luts,autogenRegs,autogenLuts);
 [regs,autogenRegs,autogenLuts] = Pipe.RAST.FRMW.bootCalcs(regs,luts,autogenRegs,autogenLuts);
 [regs,autogenRegs,autogenLuts] = Pipe.DCOR.FRMW.bootCalcs(regs,luts,autogenRegs,autogenLuts);
@@ -12,11 +12,22 @@ function [regs,autogenRegs,autogenLuts] = bootCalcs(regs,luts,autogenRegs,autoge
 
 end
 
-function [regs,autogenRegs] = generalRegisters(regs,autogenRegs)
+function [regs,autogenRegs,autogenLuts] = generalRegisters(regs,autogenRegs,autogenLuts)
 autogenRegs.GNRL.tmplLength = uint16(double(regs.GNRL.codeLength)*double(regs.GNRL.sampleRate));
 autogenRegs.GNRL.zNorm = single(bitshift(1,regs.GNRL.zMaxSubMMExp));
 autogenRegs.EXTL.auxPItxCode=regs.FRMW.txCode;
 autogenRegs.EXTL.auxPItxCodeLength=uint32(regs.GNRL.codeLength);
 regs = FirmwareBase.mergeRegs(regs,autogenRegs);
+if regs.FRMW.fovExpanderValid
+    fovExpanderLut.FRMW.fovExpander = createFeLut();
+    autogenLuts = Firmware.mergeRegs(autogenLuts,fovExpanderLut);
+end
 end
 
+function fovExpanderLut = createFeLut()
+current_dir = mfilename('fullpath');
+ix = strfind(current_dir, '\');
+path = fullfile(current_dir(1:ix(end)-1), '..\scripts\IV2calibTool\calibParams.xml');
+calibParams = xml2structWrapper(path);
+fovExpanderLut = typecast(reshape(single(calibParams.fovExpander.table), length(calibParams.fovExpander.table)*2,1),'uint32');
+end
