d5656a3915cce00bba7f79bb7aa9c4b74f678461
tbenshab <tzachi.ben.shabat@intel.com>
2019-05-26 23:19:49 +0300

    turn_in bug test

--- +Calibration/runCalibStream.m
+++ +Calibration/runCalibStream.m
@@ -48,8 +48,10 @@ function  [calibPassed] = runCalibStream(runParamsFn,calibParamsFn, fprintff,spa
     updateInitConfiguration(hw,fw,fnCalib,runParams,calibParams);
     
     %% call HVM_cal_init
-    cal_output_dir = fileparts(fopen(app.m_logfid));
-    [calibParams , ~] = HVM_Cal_init(calibParamsFn,fprintff,cal_output_dir);
+ %   cal_output_dir = fileparts(fopen(app.m_logfid));
+ %   [calibParams , ~] = HVM_Cal_init(calibParamsFn,fprintff,cal_output_dir);
+    [calibParams , ~] = HVM_Cal_init(calibParamsFn,fprintff);
+
     
     %% Start stream to load the configuration
     Calibration.aux.collectTempData(hw,runParams,fprintff,'Before starting stream:');
