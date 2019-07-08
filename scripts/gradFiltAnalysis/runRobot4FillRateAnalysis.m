27640124c87a75d1e7e121dfe5c2a668a6fb509a
mayakip <maya.kiperwas.gal@intel.com>
2019-07-08 17:21:54 +0300

    Bug fix in robot script

--- scripts/gradFiltAnalysis/runRobot4FillRateAnalysis.m
+++ scripts/gradFiltAnalysis/runRobot4FillRateAnalysis.m
@@ -1,5 +1,5 @@
 % Definitions
-distances = 20:10:300; %In cm
+distances = [60:20:200, 210:10:350]; %In cm
 savePath = 'X:\Data\gradFilt';
 depthControlState = 1;
 numOffFrames = 100;
@@ -15,16 +15,16 @@ for k = 1:length(distances)
     hw.cmd('mwd a00e166c a00e1670 00000000 // JFILgrad2bypass');
     hw.shadowUpdate();
     moveRobot2Dist(distances(k));
-    saveFrames(hw, distances(k), savePath, 'default');
+    saveFrames(hw, distances(k), savePath, 'default',numOffFrames);
     %%
     hwCommand = 'mwd a00e1600 a00e1604 00000100 // JFILgrad1thrMaxDiag';
     setOnlyOneGrad1Th(hw, hwCommand);
-    saveFrames(hw, distances(k), savePath, 'newConfig');
+    saveFrames(hw, distances(k), savePath, 'newConfig',numOffFrames);
 end
 hw.stopStream;
 
 function [status,result] = moveRobot2Dist(dist)
-command = ['plink.exe robot@ev3dev algo_ev3/move_by_target.py -t wall_10Reflectivity -d ' num2str(dist) 'cm -a 0'];
+command = ['plink.exe robot@ev3dev -pw maker -no-antispoof -noagent algo_ev3/move_by_target.py -t wall_10Reflectivity -d ' num2str(dist) 'cm -a 0'];
 [status,result] = system(command);
 if contains(result,'Host does not exist')
     [status,result] = system(command);
@@ -36,7 +36,7 @@ end
 pause(60);
 end
 
-function [] = saveFrames(hw, dist, savePath, name)
+function [] = saveFrames(hw, dist, savePath, name, numOffFrames)
     frame = hw.getFrame(10,false); % Reset the buffer
     frame = hw.getFrame(numOffFrames,false);
     str = sprintf([name '_dist_%03d_cm'],dist);
