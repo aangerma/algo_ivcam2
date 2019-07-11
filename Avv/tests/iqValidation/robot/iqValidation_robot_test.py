5ac9c0333d2624f98ebb9563d6957892c24dc922
yresnick <yoni.resnick@intel.com>
2019-07-11 10:09:47 +0300

    lib real sense debug

--- Avv/tests/iqValidation/robot/iqValidation_robot_test.py
+++ Avv/tests/iqValidation/robot/iqValidation_robot_test.py
@@ -40,6 +40,10 @@ def test_validation_robot_time_drift():
     slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
     runIqValidation.validation_test(filePath)
 
+def test_validation_DEBUG():
+    filePath = r'Avv/tests/iqValidation/debug.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validation_test(filePath)
 
 if __name__ == "__main__":
     test_validation_robot_regression()
