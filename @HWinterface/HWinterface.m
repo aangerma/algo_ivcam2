d672f9909e7d56368015c0a0e4cae23330071f16
heliyahu <hila.eliyahu@intel.com>
2019-07-21 18:15:05 +0300

    save file

--- @HWinterface/HWinterface.m
+++ @HWinterface/HWinterface.m
@@ -233,7 +233,7 @@ classdef HWinterface <handle
         function saveRecData(obj)
             if(~isempty(obj.m_recfn))
                 recData = obj.m_recData;%#ok
-                save(obj.m_recfn,'recData','-v7.3');
+                save(obj.m_recfn,'recData');
             end
         end
         %destructor
