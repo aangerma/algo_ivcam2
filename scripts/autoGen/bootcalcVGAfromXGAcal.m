e6e908d2139e0d7ca00124b8d9fd8c6f0585b145
heliyahu <hila.eliyahu@intel.com>
2019-07-22 10:53:42 +0300

    xga

--- scripts/autoGen/bootcalcVGAfromXGAcal.m
+++ scripts/autoGen/bootcalcVGAfromXGAcal.m
@@ -1,5 +1,5 @@
 % load XGA calib
-fw_XgaCalib=Pipe.loadFirmware('X:\Users\hila\FWBootCalcs\XGA\Algo1\PC17\AlgoInternal');
+fw_XgaCalib=Pipe.loadFirmware('X:\Users\hila\FWBootCalcs\XGA\fw\withCoarseMaskingAndPreset\New folder\PC22\AlgoInternal');
 RegsXGAstream=fw_XgaCalib.get(); 
 % change to VGA (RES + relevent config)
 GenralVGAfw=Pipe.loadFirmware('C:\GIT\ALGO_Projects\algo_ivcam2\+Calibration\releaseConfigCalibVGA'); 
@@ -13,7 +13,11 @@ newRegs.FRMW.cbufMargin=VGAregs.FRMW.cbufMargin;
 newRegs.FRMW.externalHsize=VGAregs.FRMW.externalHsize;
 newRegs.FRMW.externalVsize=VGAregs.FRMW.externalVsize;
 newRegs.JFIL.irShadingScale=VGAregs.JFIL.irShadingScale; 
-fw_XgaCalibVgastream=Pipe.loadFirmware('X:\Users\hila\FWBootCalcs\XGA\Algo1\PC17\AlgoInternal');;
+% set preset regs for dcor masking
+newRegs.FRMW.algoThermalLoopOffset=single(56.2560296343196); 
+newRegs.FRMW.coarseMaskingMax=uint16(20); 
+%%
+fw_XgaCalibVgastream=Pipe.loadFirmware('X:\Users\hila\FWBootCalcs\XGA\fw\withCoarseMaskingAndPreset\New folder\PC22\AlgoInternal');
 fw_XgaCalibVgastream.setRegs(newRegs,''); 
 % autogen for vga with xga calib
 RegsVGAstream=fw_XgaCalibVgastream.get(); 
