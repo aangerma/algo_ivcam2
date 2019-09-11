71d60c8d11bbce4346e8fb366a1db11c3ec565db
mayakip <maya.kiperwas.gal@intel.com>
2019-09-11 15:43:09 +0300

    Chnaging the CalibParams to calibrate RGB with distortion modeling and adjusted validation to show UV mapping and RGB line fit

--- +Calibration/+validation/validateRGB.m
+++ +Calibration/+validation/validateRGB.m
@@ -16,6 +16,8 @@ intr = typecast(b,'single');
 Krgb = eye(3);
 % Verify the correct K for resolution used
 Krgb([1,5,7,8,4]) = intr([calibParams.validationConfig.rgb.startIxRgb:calibParams.validationConfig.rgb.startIxRgb+3,1]);%intr([6:9,1]);
+drgb = intr(calibParams.validationConfig.rgb.startIxRgb+4:calibParams.validationConfig.rgb.startIxRgb+8);
+
 [ ~,b] = hw.cmd('RGB_EXTRINSICS_GET');
 extr = typecast(b,'single');
 Rrgb = reshape(extr(1:9),[3 3])';
@@ -27,13 +29,14 @@ params.sampleZFromWhiteCheckers = calibParams.validationConfig.sampleZFromWhiteC
 params.validateOnCenter = calibParams.validationConfig.validateOnCenter;
 params.roi = calibParams.validationConfig.roi4ValidateOnCenter;
 params.isRoiRect = calibParams.validationConfig.gidMaskIsRoiRect;
+params.rgbDistort = drgb;
+params.Krgb = Krgb;
+params.Krgbn = du.math.normalizeK(Krgb,calibParams.rgb.imSize);
 %%
 if params.sampleZFromWhiteCheckers
-    [~, resultsUvMapWht,dbgWht] = Validation.metrics.uvMapping(depthFrame, params, rgbFrame.color);
-    [resultsLineFitWht] = Calibration.aux.calcLineDistortion({dbgWht.vertices},double(Krgb),dbgWht.gridSize);
-    [resultsWht] = arrangResultStruct( resultsLineFitWht,resultsUvMapWht, 'Wht');
+    [resultsWht,dbgWht] = runUVandLF( depthFrame, params, rgbFrame.color, 'Wht');
     params.sampleZFromWhiteCheckers = 0;
-    
+    %%
     ff = Calibration.aux.invisibleFigure();
     imagesc(rgbFrame.color);hold on;
     scatter(dbgWht.sampledCornerRGB(:,1),dbgWht.sampledCornerRGB(:,2),'w');
@@ -42,10 +45,8 @@ if params.sampleZFromWhiteCheckers
     title('Validation RGB UV mapping image: white is sampled and black is mapped points - for z sampled from white');
     Calibration.aux.saveFigureAsImage(ff,runParams,'Validation','rgbUvMapImageFromWht',1);
 end
-[~, resultsUvMapReg,dbgReg] = Validation.metrics.uvMapping(depthFrame, params, rgbFrame.color);
-[resultsLineFitReg] = Calibration.aux.calcLineDistortion({dbgReg.vertices},double(Krgb),dbgReg.gridSize);
-[resultsReg] = arrangResultStruct( resultsLineFitReg,resultsUvMapReg, 'Reg');
-
+[resultsReg,dbgReg] = runUVandLF( depthFrame, params, rgbFrame.color, 'Reg');
+%%
 ff = Calibration.aux.invisibleFigure();
 imagesc(rgbFrame.color);hold on;
 scatter(dbgReg.sampledCornerRGB(:,1),dbgReg.sampledCornerRGB(:,2),'w');
@@ -64,39 +65,28 @@ frames.color = rgbFrame.color;
 
 end
 
-
-function [newStr] = editStr( origStr,string2erase,string2add)
-    newStr = erase(origStr,string2erase);
-    newStr = strcat(newStr,string2add);
-end
-
 function [results] = arrangResultStruct( resultsLineFit,resultsUvMap, postFix)
-fields = fieldnames(resultsLineFit);
-for k = 1:length(fields)
-    if contains(fields{k},'3D')
-        resultsLineFit = rmfield(resultsLineFit,fields{k});
-        continue;
-    end
-    string2erase = 'ErrorTotalHoriz2D';
-    if contains(fields{k},string2erase)
-        string2add = 'ErrHor2dRGB';
-        [newName] = editStr( fields{k},string2erase,string2add);
-        results.(strcat(newName,postFix)) = resultsLineFit.(fields{k});
-         continue;
-    end
-    string2erase = 'ErrorTotalVertic2D';
-    if contains(fields{k},string2erase)
-        string2add = 'ErrVer2dRGB';
-        [newName] = editStr( fields{k},string2erase,string2add);
-        results.(strcat(newName,postFix)) = resultsLineFit.(fields{k});
-        continue;
-    end
-end
-
+results.(strcat('lineFitRmsErrHor2dRGB',postFix)) = resultsLineFit.lineFitRmsErrorTotal_h;
+results.(strcat('lineFitRmsErrVer2dRGB',postFix)) = resultsLineFit.lineFitRmsErrorTotal_v;
+results.(strcat('lineFitMaxErrHor2dRGB',postFix)) = resultsLineFit.lineFitMaxErrorTotal_h;
+results.(strcat('lineFitMaxErrVer2dRGB',postFix)) = resultsLineFit.lineFitMaxErrorTotal_v;
 results.(strcat('uvMapRmse',postFix)) = resultsUvMap.rmse;
 results.(strcat('uvMapMaxErr',postFix)) = resultsUvMap.maxErr;
 results.(strcat('uvMapMaxErr95',postFix)) = resultsUvMap.maxErr95;
 results.(strcat('uvMapMinErr',postFix)) = resultsUvMap.minErr;
+end
 
 
+function [results,dbg] = runUVandLF( depthFrame, params, rgbFrame, postFix)
+[~, resultsUvMap,dbg] = Validation.metrics.uvMapping(depthFrame, params, rgbFrame);
+pts = cat(3,dbg.cornersRGB(:,:,1),dbg.cornersRGB(:,:,2),zeros(size(dbg.cornersRGB,1),size(dbg.cornersRGB,2)));
+pts = Calibration.aux.CBTools.slimNans(pts);
+if isfield(params,'rgbDistort')
+    invd = du.math.fitInverseDist(params.Krgbn,params.rgbDistort);
+    pixsUndist = du.math.distortCam(reshape(pts(:,:,1:2),[],2)', params.Krgb, invd);
+    ptsUndist = cat(3,reshape(pixsUndist',size(pts,1),size(pts,2),[]),pts(:,:,3));
+    pts = ptsUndist;
+end
+[resultsLineFit] = Validation.metrics.get3DlineFitErrors(pts);
+[results] = arrangResultStruct( resultsLineFit,resultsUvMap, postFix);
 end
