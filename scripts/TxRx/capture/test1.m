a5b847893a37a14d500c1c1b631f7050b765690b
heliyahu <hila.eliyahu@intel.com>
2019-09-22 11:51:37 +0300

    test

--- scripts/TxRx/capture/test1.m
+++ scripts/TxRx/capture/test1.m
@@ -1,13 +1,17 @@
-dist_num=450;
-code_i=6;
-outputFolder='X:\Users\hila\L520\TxRx\CodeTestData'; 
+clear all; close all; 
+dist_num=1500;
+load('X:\Users\hila\L520\TxRx\CodesStruct.mat');
+
+
+outputFolder='X:\Users\hila\L520\TxRx\CodeTestData\Test1';
 warning off
 hw = HWinterface();
 hw.cmd('dirtybitbypass');
-hw.runScript('C:\GIT\AlgoProjects\algo_ivcam2\scripts\TxRx\capture\ldOn.txt');
+hw.runScript('X:\Users\tomer\FG\ldOn.txt');
 pause(1);
-
-load('X:\Users\hila\L520\TxRx\CodesStruct.mat');
+%%
+% for(code_i=1:1:length(codes))
+code_i=13;
 code=codes(code_i).code;
 
 code2Use = uint32(codes(code_i).txCodeRegDec)';
@@ -20,8 +24,9 @@ hw.writeAddr(uint32(2684682680),uint32(length(code)));% EXTLauxPItxCodeLength
 hw.cmd('mwd a00d01ec a00d01f0 00000111 // EXTLauxShadowUpdateFrame');
 pause(0.1);
 % hw.read('EXTLauxPItxCode')
-
-ivs=Utils.FG_IVS(outputFolder,1);
+out=strcat(outputFolder,'\FG_',num2str(dist_num),'\',codes(code_i).name);
+mkdirSafe(out);
+ivs=Utils.FG_IVS(out,1);
 hw.cmd('rst');
 
 FlagsCodeStartMask = bitget(ivs.flags,2);
@@ -45,10 +50,11 @@ for chunk=1:length(flag_indexes)
     chunk_start_fast=(flag_indexes(chunk)-1)*64+2;
     for i=1:code_per_chunk
         fast(:,total_code)=ivs.fast(chunk_start_fast+fast_size*(i):chunk_start_fast+fast_size*(i+1)-1)';
-        slow(:,total_code)=ivs.slow(chunk_start_slow+slow_size*(i):chunk_start_slow+slow_size*(i+1)-1)'; 
+        slow(:,total_code)=ivs.slow(chunk_start_slow+slow_size*(i):chunk_start_slow+slow_size*(i+1)-1)';
         total_code=total_code+1;
     end
 end
+h=figure();
 imagesc(fast(:,1:1000));
 
 
@@ -56,3 +62,8 @@ dist=dist_num*ones(1,total_code_amount);
 dname=strcat(outputFolder,'\',num2str(dist_num));
 mkdirSafe(dname);
 save(strcat(outputFolder,'\',num2str(dist_num),'\',codes(code_i).name,'.mat' ),'fast','slow','dist');
+saveas(h,strcat(outputFolder,'\',num2str(dist_num),'\',codes(code_i).name,'.png' ));
+
+pause(2);
+hw.cmd('rst');
+% end
