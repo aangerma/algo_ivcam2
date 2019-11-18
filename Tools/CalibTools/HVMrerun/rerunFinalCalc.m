d7993de7718fd650a6c827b7d247d867c97d2893
syaeli <steve.yaeli@intel.com>
2019-11-18 15:17:26 +0200

    adding rerun script

---
+++ Tools/CalibTools/HVMrerun/rerunFinalCalc.m
@@ -0,0 +1,24 @@
+clear all
+clc
+
+atcPath = '\\143.185.124.250\Tester-data\IDC Data\IVCAM\L515\Calibration\BIG PBS\HENG-2693\F9340026\Avishayexperiment\18-11\3.7.0.0 ATC\';
+atcFolder = [atcPath, 'Matlab\mat_files\'];
+calibDir = [atcPath, '\Matlab\AlgoInternal'];
+
+%%
+
+fprintf('running finalCalcAfterHeating...\n');
+dataIn = load([atcFolder, 'finalCalcAfterHeating_in.mat']);
+% eternal overrides
+dataIn.calib_dir = calibDir;
+dataIn.output_dir = 'D:/temp';
+dataIn.runParams.outputFolder = dataIn.output_dir;
+dataIn.fprintff = @fprintf;
+% ad-hoc overrides
+dataIn.calibParams.fwTable.nRowsRGB = 29;
+% rerun
+[dataRes.data, dataRes.calibPassed, dataRes.results, dataRes.metrics, dataRes.metricsWithTheoreticalFix, dataRes.Invalid_Frames] = Calibration.thermal.finalCalcAfterHeating(dataIn.data, dataIn.eepromRegs, dataIn.calibParams, dataIn.fprintff, dataIn.calib_dir, dataIn.runParams);
+dataOut = load([atcFolder, 'finalCalcAfterHeating_out.mat']);
+checkOutputEquality(dataOut, dataRes)
+fprintf('\n')
+
