60fa2c1eb690333b7d3bd4f759b3ea60313c3297
mayakip <maya.kiperwas.gal@intel.com>
2019-12-02 11:25:56 +0200

    Latest version from Hila

--- scripts/projectionFovLutScripts/createFovLut/calcProjRectScatterInterp.m
+++ scripts/projectionFovLutScripts/createFovLut/calcProjRectScatterInterp.m
@@ -25,9 +25,12 @@ six=scatteredInterpolant(double(xF),double(yF),double(angx(:)));
 siy=scatteredInterpolant(double(xF),double(yF),double(angy(:)));
 
 %% addaption to have Full valid fov - should be configuration for all units
-buffTop =linspace(0,-35,1024)'-6;%added symetric 5 up
-buffBottom=[linspace(25,15,1024/4)';ones(768,1)*15]+11; %5;
-buffleft=-70;
+n1=round(190);
+buffTop =[ones(n1,1)*-8;linspace(-8,-35,1024-n1)';]-8;
+n1=280;
+n2=185;
+buffBottom=[linspace(32,18,n1)';ones(n2,1)*18;linspace(18,28,1024-n1-n2)']+3; 
+buffleft=0;
 buffright=0;
 %%
 figure(); hold all; 
@@ -55,32 +58,75 @@ plot(VBangx+2047,VBangy+2047);
 legend('H left', 'H right','V top','V Bottom');   title('angy vs angx');  grid minor;  
 hold off; 
 
-%%
+%% before ears cliping
+% vmin=VTangy+2047; 
+% upsample = @(x,n)(vec(repmat(x(:),1,4)'));
+% vmin = upsample((vmin))';
+% % lmargin=min(HLangx+2047); 
+% % rmargin=max(HRangx+2047);
+% rmargin=4096-220;%220 
+% lmargin = 10;%10;
+% vmin(1:round(lmargin))=0; 
+% vmin(round(rmargin):end)=0; 
+% 
+% vmax=VBangy+2047; 
+% vmax = upsample((vmax))';
+% vmax(1:round(lmargin))=0;
+% vmax(round(rmargin):end)=0;
+% 
+% 
+% figure(); plot(vmin); hold all; plot(vmax); legend('vmin','vmax');
+
+%% after ears cliping
+%left lobe
+[lmargin, lmarginix]=min(HLangx+2047);
+figure(); hold on; plot(HLangx(1:lmarginix)+2047,HLangy(1:lmarginix)+2047); plot(HLangx(lmarginix:end)+2047,HLangy(lmarginix:end)+2047); legend; 
+
+buttomLobex=HLangx(1:lmarginix)+2047; 
+buttomLobey=HLangy(1:lmarginix)+2047;
+
 vmin=VTangy+2047; 
 upsample = @(x,n)(vec(repmat(x(:),1,4)'));
 vmin = upsample((vmin))';
-%lmargin=min(HLangx+2047); 
-% rmargin=max(HRangx+2047);
-rmargin=4096-220; 
-lmargin = 10;
-vmin(1:round(lmargin))=0; 
-vmin(round(rmargin):end)=0; 
+buttomLobeAligned = accumarray( round(buttomLobex),buttomLobey,[4096 1],@mean);
+buttomLobeAligned(buttomLobeAligned==0) = nan;
+mxI = maxind(buttomLobeAligned);
+mnI = minind(buttomLobeAligned);
+buttomLobeAligned(mxI+1:mnI-1) = smooth1D_(mxI+1:mnI-1,buttomLobeAligned(mxI+1:mnI-1),5);
+buttomLobeAligned(isnan(buttomLobeAligned)) = 0;
+%vmin = max(vmin,buttomLobeAligned');
+vmin(1:round(lmargin)-1)=0;
+%% right lobe
+[rmargin, rmarginix]=max(HRangx+2047);
+offsetShift=max(rmargin-4096,0)+20; % 220-113 margin to modify
+buttomLobex=HRangx(1:rmarginix)+2047-offsetShift; 
+buttomLobey=HRangy(1:rmarginix)+2047;
+figure(); plot(buttomLobex,buttomLobey);
+
+buttomLobeAlignedR = accumarray( round(buttomLobex),buttomLobey,[4096 1],@mean);
+buttomLobeAlignedR(buttomLobeAlignedR==0) = nan;
+mnI = maxind(buttomLobeAlignedR);
+mxI = minind(buttomLobeAlignedR);
+buttomLobeAlignedR(mxI:mnI-1) = smooth1D_(mxI:mnI-1,buttomLobeAlignedR(mxI:mnI-1),5);
+buttomLobeAlignedR(isnan(buttomLobeAlignedR)) = 0;
+vmin = max(vmin,buttomLobeAlignedR');
+%%
+leftShift=113; %123-10 margin 2 modify
+vmin=circshift(vmin,-leftShift); 
+vmin(round(mnI-leftShift):end)=0;
 
 vmax=VBangy+2047; 
 vmax = upsample((vmax))';
-vmax(1:round(lmargin))=0;
-vmax(round(rmargin):end)=0;
+%vmax = min(vmax,4096-buttomLobeAligned');
+vmax = min(vmax,4096-buttomLobeAlignedR');
 
-% vmax1=4095-vmin; 
-% vmin1 = 4095-vmax;
-% vmin1(vmin==0) = 0;
-% vmax1(vmax==0) = 0
-% vmin = vmin1;
-% vmax = vmax1;
+
+vmax(1:round(lmargin))=0;
+vmax=circshift(vmax,-leftShift); 
+vmax(round(mnI-leftShift):end)=0;
 
 
 figure(); plot(vmin); hold all; plot(vmax); legend('vmin','vmax');
-
 %%
 %% write FOV LUT table 
 vs =double(vec([round(vmin);round(vmax)]));
@@ -92,9 +138,9 @@ vshFull(2:4:end,:) = vsh(2:3:end,:);
 vshFull(3:4:end,:) = vsh(1:3:end,:);
 
 %% write script to txt file
-fpath='X:\Users\hila\L515\projectionByRoiLut\fullTest_release\FOVlut\lut_6bottom6top'; 
+fpath='X:\Users\hila\L515\projectionByRoiLut\fullTest_release\FOVlut\fixingEars\removeEarsLuts\clipingEarsRightOnly'; 
 mkdirSafe(fpath); 
-fileName='FovLut.txt'; 
+fileName='FOVlut.txt'; 
 fileID = fopen(fileName,'w');
 
 n=4096; %32
@@ -110,3 +156,22 @@ copyfile(fileName,fullfile(fpath,fileName),'f');
 
 %% write table to bin
 writeTableToBin(vshFull, fpath); 
+%%
+function yy = smooth1D_(x,y,sig)
+    y=y(:);
+    if(isempty(x))
+        x= (1:length(y))';
+    else
+        x=x(:);
+    end
+    n = length(y);
+    if(isempty(x))
+        x=1:n;
+    end
+    yy = zeros(n,1);
+    for i=1:n;
+        ker = exp(-0.5/sig^2*(x-x(i)).^2);
+        ker = ker/sum(ker);
+        yy(i) = nansum(y(:).*ker(:));
+    end
+end
