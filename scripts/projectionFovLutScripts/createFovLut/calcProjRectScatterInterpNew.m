044a5e603ceffd1b66fc2a1ed4a9657b246ac33b
mayakip <maya.kiperwas.gal@intel.com>
2019-12-09 16:41:54 +0200

    WIP

--- scripts/projectionFovLutScripts/createFovLut/calcProjRectScatterInterpNew.m
+++ scripts/projectionFovLutScripts/createFovLut/calcProjRectScatterInterpNew.m
@@ -8,8 +8,15 @@ load('X:\Users\mkiperwa\projection\F9340671\Cal TM2 3.9.2.0\Matlab\AlgoInternal\
 load('X:\Users\mkiperwa\projection\F9340671\rectData.mat')
  
 verbose = 1; 
-topMargin = 100;
-bottomMargin = 100;
+topMarginPix = 100;
+bottomMarginPix = 100;
+totRange = 4095;
+rectY(1) = 0;
+rectY(2) = 480;
+rectX(3) = 0;%-88;
+rectX(4) = 1024;%1139;
+
+
 %% X,Y to angle using scatteredInterpolant from ang to x,y (using udist tps model relevant to unit)
 anglesRng = 2047;
 [angx,angy] = meshgrid(linspace(-anglesRng,anglesRng,100));
@@ -31,24 +38,20 @@ f2AngX=scatteredInterpolant(double(xF),double(yF),double(angx(:)));
 f2AngY=scatteredInterpolant(double(xF),double(yF),double(angy(:)));
 
 %%
-[angRect] = calcRectInAngleDim(f2AngX,f2AngY,rectX,rectY,[xF,yF],regs,tpsUndistModel,anglesRng,verbose);
+[angRect] = calcRectInAngleDim(f2AngX,f2AngY,rectX,rectY,[xF,yF],regs,tpsUndistModel,anglesRng,totRange,verbose);
 %%
-[lmargin, lmarginix]=min(angRect.HLang.x+anglesRng);
+%{
 if verbose
     figure(); hold on;
     plot(angRect.HLang.x(1:lmarginix)+anglesRng,angRect.HLang.y(1:lmarginix)+anglesRng);
     plot(angRect.HLang.x(lmarginix:end)+anglesRng,angRect.HLang.y(lmarginix:end)+anglesRng); legend;
     axis ij;
 end
+%}
+vmin = (angRect.VTang.y+(anglesRng - 100))*anglesRng/(anglesRng - 100); 
 
-vmin=fliplr(4095-angRect.VBang.y+anglesRng); 
-upsample = @(x,n)(vec(repmat(x(:),1,4)'));
-vmin = upsample((vmin))';
-vmin(1:round(lmargin)-1)=0;
 %%
-vmax=fliplr(4095-angRect.VTang.y+anglesRng); 
-vmax = upsample((vmax))';
-vmax(1:round(lmargin))=0;
+vmax = (angRect.VBang.y+(anglesRng - 100))*anglesRng/(anglesRng - 100); 
 if verbose
     figure(); plot(vmin); hold all; plot(vmax); legend('vmin','vmax'); axis ij;
 end
@@ -57,15 +60,17 @@ end
 % vmin=zeros(1,4096);vmin(500:2000)=100;vmin(3000:3500)=100;
 % vmax=zeros(1,4096);vmax(500:2000)=4090;vmax(3000:3500)=4090;
 % vmin(1:1000) = 0;
-%{
-vminOrig = vmin;
-vmin = 4095-vmax;
-vmax = 4095-vminOrig;
-%}
+
+vminOrig = vmin';
+vmaxOrig = vmax';
+vmin = fliplr(totRange-vmaxOrig);
+vmax = fliplr(totRange-vminOrig);
+% vmin = vminOrig;
+% vmax = vmaxOrig;
 
 vs =double(vec([round(vmin);round(vmax)]));
 vs8 = reshape(vs,8,[]);
-vsh = reshape(vec(dec2hex(bitand(vs8,4095),3)'),8,[])';
+vsh = reshape(vec(dec2hex(bitand(vs8,totRange),3)'),8,[])';
 vshFull = repmat('0',4096,8);
 vshFull(1:4:end,:) = vsh(3:3:end,:);
 vshFull(2:4:end,:) = vsh(2:3:end,:);
@@ -102,8 +107,8 @@ vNew = Calibration.Undist.undistByTPSModel( vNew',tpsUndistModel )';% 2D Undist
 end
 
 
-function [angRect] = calcRectInAngleDim(f2AngX,f2AngY,rectX,rectY,pincushPts,regs,tpsUndistModel,anglesRng,verbose)
-
+function [angRect] = calcRectInAngleDim(f2AngX,f2AngY,rectX,rectY,pincushPts,regs,tpsUndistModel,anglesRng,totRange,verbose)
+%{
 % Horizontal left
 angRect.HLang.x=f2AngX(double(ones(regs.GNRL.imgVsize,1)*rectX(3)),double((1:regs.GNRL.imgVsize)'));
 angRect.HLang.y=f2AngY(double(ones(regs.GNRL.imgVsize,1)*rectX(3)),double((1:regs.GNRL.imgVsize)'));
@@ -111,7 +116,6 @@ angRect.HLang.y=f2AngY(double(ones(regs.GNRL.imgVsize,1)*rectX(3)),double((1:reg
 if verbose
     figure(120784);hold on; axis ij; plot(angRect.HLang.x+anglesRng,angRect.HLang.y+anglesRng);
     [xFnew,yFnew] = inverseAngs(angRect.HLang.x,angRect.HLang.y,regs,tpsUndistModel);
-    figure(151285);axis ij;scatter(pincushPts(:,1),pincushPts(:,2));grid minor;
     hold on; scatter(xFnew(:),yFnew(:));grid minor;
 end
 %%
@@ -125,19 +129,40 @@ if verbose
     [xFnew,yFnew] = inverseAngs(angRect.HRang.x,angRect.HRang.y,regs,tpsUndistModel);
     figure(151285);scatter(xFnew(:),yFnew(:));grid minor;
 end
-
+%}
+nPts = 100;
 % vertical top
-angRect.VTang.x=f2AngX(double((1:regs.GNRL.imgHsize)'),double(ones(regs.GNRL.imgHsize,1)*rectY(1)));
-angRect.VTang.y=f2AngY(double((1:regs.GNRL.imgHsize)'),double(ones(regs.GNRL.imgHsize,1)*rectY(1)));
+xVals = double(linspace(rectX(3),rectX(4),nPts));
+Ymid = rectY(2)/2;
+angXMiddleY = f2AngX(xVals',double(ones(numel(xVals),1)*Ymid));
+[angYgrid,angXgrid] = ndgrid(linspace(-anglesRng,anglesRng,numel(angXMiddleY)),angXMiddleY); % Creating a grid in the angle domain along the scan lines
+[xValsNew,yValsNew] = inverseAngs(angXgrid,angYgrid,regs,tpsUndistModel);
+[minVal1,ix1] = min(abs(yValsNew(1:nPts)-rectY(1)));
+[minVal2,ix2] = min(abs(yValsNew(end-nPts+1:end)-rectY(1)));
+tempX = xValsNew(1:nPts);
+minX = tempX(ix1);
+tempX = xValsNew(end-nPts+1:end);
+maxX = tempX(ix2);
+
+angRect.VTang.x = f2AngX(double(linspace(minX,maxX,totRange+1)'),double(ones(totRange+1,1)*rectY(1)));
+angRect.VTang.y = f2AngY(double(linspace(minX,maxX,totRange+1)'),double(ones(totRange+1,1)*rectY(1)));
 if verbose
     figure(120784);plot(angRect.VTang.x+anglesRng,angRect.VTang.y+anglesRng);
     [xFnew,yFnew] = inverseAngs(angRect.VTang.x,angRect.VTang.y,regs,tpsUndistModel);
-    figure(151285);scatter(xFnew(:),yFnew(:));grid minor;
+    figure(151285);axis ij;
+    scatter(pincushPts(:,1),pincushPts(:,2));grid minor;
+    hold on; scatter(xFnew(:),yFnew(:));grid minor;
 end
 
 % vertical Bottom
-angRect.VBang.x=f2AngX(double((1:regs.GNRL.imgHsize)'),double(ones(regs.GNRL.imgHsize,1)*rectY(2)));
-angRect.VBang.y=f2AngY(double((1:regs.GNRL.imgHsize)'),double(ones(regs.GNRL.imgHsize,1)*rectY(2)));
+[minVal1,ix1] = min(abs(yValsNew(1:nPts)-rectY(2)));
+[minVal2,ix2] = min(abs(yValsNew(end-nPts+1:end)-rectY(2)));
+tempX = xValsNew(1:nPts);
+minX = tempX(ix1);
+tempX = xValsNew(end-nPts+1:end);
+maxX = tempX(ix2);
+angRect.VBang.x = f2AngX(double(linspace(minX,maxX,totRange+1)'),double(ones(totRange+1,1)*rectY(2)));
+angRect.VBang.y = f2AngY(double(linspace(minX,maxX,totRange+1)'),double(ones(totRange+1,1)*rectY(2)));
 if verbose
     figure(120784);plot(angRect.VBang.x+anglesRng,angRect.VBang.y+anglesRng);
     legend('H left', 'H right','V top','V Bottom');   title('angy vs angx');  grid minor;
