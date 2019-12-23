c1193c938988a5ebde4f70cb53556e542ee32ee5
syaeli <steve.yaeli@intel.com>
2019-12-23 10:31:20 +0200

    Robustify Z_DelayCalibCalc against high mirror freq

--- Tools/CalibTools/AlgoCameraCalibration/AlgoCameraCalibToolVersion.m
+++ Tools/CalibTools/AlgoCameraCalibration/AlgoCameraCalibToolVersion.m
@@ -4,7 +4,7 @@ function [vers,subVersion,versionBytes] = AlgoCameraCalibToolVersion()
         vers = 3.01;
         subVersion = 0;
     else
-        vers = 4.30;
+        vers = 4.29;
         subVersion = 0;
     end
     
