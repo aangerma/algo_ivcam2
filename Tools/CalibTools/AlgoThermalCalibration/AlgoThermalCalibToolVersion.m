ac412f4183859e40154b5a2061c8ae1265838c88
mayakip <maya.kiperwas.gal@intel.com>
2020-02-05 11:23:27 +0200

    Fixed relative Error Metrics (commiting for Tal)

--- Tools/CalibTools/AlgoThermalCalibration/AlgoThermalCalibToolVersion.m
+++ Tools/CalibTools/AlgoThermalCalibration/AlgoThermalCalibToolVersion.m
@@ -4,7 +4,7 @@ function [vers, subVersion] = AlgoThermalCalibToolVersion()
         vers = 3.01;
         subVersion = 0;
     else
-        vers = 4.39;
+        vers = 4.40;
         subVersion = 0;
     end
 end
