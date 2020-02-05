de822a3420fce61c4f24261a71c422b590c98179
mayakip <maya.kiperwas.gal@intel.com>
2020-02-05 15:32:31 +0200

    Derivative for online calibration - update

--- scripts/onlineCalibration/calcSymDerivatives.m
+++ scripts/onlineCalibration/calcSymDerivatives.m
@@ -1,6 +1,7 @@
 syms X Y Z
-A = sym('A',[3,4]);
-Krgb = sym('Krgb',[3,3]);
+
+A = sym('a',[3,4]);
+Krgb = sym('krgb',[3,3]);
 d = sym('d',[1,5]);
 %%
 
@@ -17,17 +18,44 @@ dYin_dA = simplify([ diff(y_in,A(1,1)),diff(y_in,A(1,2)) , diff(y_in,A(1,3)),dif
             diff(y_in,A(2,1)) , diff(y_in,A(2,2)) , diff(y_in,A(2,3)), diff(y_in,A(2,4));...
             diff(y_in,A(3,1)) , diff(y_in,A(3,2)) , diff(y_in,A(3,3)) , diff(y_in,A(3,4))]);
 
-x1 = (x_in-Krgb(1,3))/Krgb(1,1);
-y1 = (y_in-Krgb(2,3))/Krgb(2,2);
+%%
+syms x3 y3
+x_out = x3*Krgb(1,1)+Krgb(1,3);
+y_out = y3*Krgb(2,2)+Krgb(2,3);
 
+dXout_dX3 = diff(x_out,x3);
+dYout_dY3 = diff(y_out,y3);
+%%
+syms x2 y2 r_2 x1 y1
+x3 = x2+2*d(1,3)*x1*y1+d(1,4)*(r_2+2*x1^2);
+y3 = y2+2*d(1,4)*x1*y1+d(1,3)*(r_2+2*y1^2);
+
+dX3_dX2 = diff(x3,x2);
+dY3_dY2 = diff(y3,y2);
+%%
 r_2 = x1^2+y1^2;
 rc = 1+d(1,1)*r_2+d(1,2)*r_2^2+d(1,5)*r_2^3;
 x2 = x1*rc;
 y2 = y1*rc;
-x3 = x2+2*d(1,3)*x1*y1+d(1,4)*(r_2+2*x1^2);
-y3 = y2+2*d(1,4)*x1*y1+d(1,3)*(r_2+2*y1^2);
 
-x_out = x3*Krgb(1,1)+Krgb(1,3);
-y_out = y3*Krgb(2,2)+Krgb(2,3);
+dX2_dX1 = diff(x2,x1);
+dY2_dY1 = diff(y2,y1);
+dX2_dY1 = diff(x2,y1);
+dY2_dX1 = diff(y2,x1); 
+%%
+syms x_in y_in 
 
-% dXout_dXin = diff(x_out,x_in);
+x1 = (x_in-Krgb(1,3))/Krgb(1,1);
+y1 = (y_in-Krgb(2,3))/Krgb(2,2);
+
+dX1_dXin = diff(x1,x_in);
+dY1_dYin = diff(y1,y_in);
+%%
+dXout_dXin = dXout_dX3*dX3_dX2*dX2_dX1*dX1_dXin;
+dYout_dYin = dYout_dY3*dY3_dY2*dY2_dY1*dY1_dYin;
+
+dXout_dYin = dXout_dX3*dX3_dX2*dX2_dY1*dY1_dYin;
+dYout_dXin = dYout_dY3*dY3_dY2*dY2_dX1*dX1_dXin;
+
+xCoeff = simplify(dXout_dXin*dXin_dA + dXout_dYin*dYin_dA);
+ycoeff = simplify(dYout_dXin*dXin_dA + dYout_dYin*dYin_dA);
