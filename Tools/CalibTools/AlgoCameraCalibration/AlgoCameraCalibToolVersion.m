ca86ef7c861d9275e8c61a3c7b6bbfbfb42ddfd3
syaeli <steve.yaeli@intel.com>
2020-02-06 15:17:32 +0200

    DLL version upgrade

--- Tools/CalibTools/AlgoCameraCalibration/AlgoCameraCalibToolVersion.m
+++ Tools/CalibTools/AlgoCameraCalibration/AlgoCameraCalibToolVersion.m
@@ -4,7 +4,7 @@ function [vers,subVersion,versionBytes] = AlgoCameraCalibToolVersion()
         vers = 3.01;
         subVersion = 0;
     else
-        vers = 4.40;
+        vers = 4.41;
         subVersion = 0;
     end
     
