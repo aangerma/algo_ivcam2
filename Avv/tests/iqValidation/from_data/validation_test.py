574a9531b2e5d19201193eaacd71be8452d56f30
Cryos7 <60876460+Cryos7@users.noreply.github.com>
2020-02-11 18:46:44 +0200

    Update validation_test.py

--- Avv/tests/iqValidation/from_data/validation_test.py
+++ Avv/tests/iqValidation/from_data/validation_test.py
@@ -6,22 +6,31 @@ sys.path.insert(0, r"..\algo_automation\tests")
 import runIqValidation
 
 
-@slash.tag('turn_in')
-def test_validation_turn_in():
-    filePath = r'Avv/tests/iqValidation/from_data/turn_in.xml'
+# IVCAM 2.0 turn in tests - mat, ivs, bin extensions supported.
+@slash.tag('turn_in_mat')
+def test_validation_turn_in_mat():
+    filePath = r'Avv\sources\validation_turn_in\Turn_in_mat\turn_in_mat.xml'
     slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
     runIqValidation.validationExecution(filePath)
 
+
+@slash.tag('turn_in_IVS')
+def test_validation_turn_in_ivs():
+    filePath = r'Avv\sources\validation_turn_in\Turn_in_IVS\turn_in_ivs.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validationExecution(filePath)
+
+
+@slash.tag('turn_in_bin')
+def test_validation_turn_in_bin():
+    filePath = r'Avv\sources\validation_turn_in\Turn_in_bin\turn_in_bin.xml'
+    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
+    runIqValidation.validationExecution(filePath)
+
+# IVCAM 2.0 turn in tests - END here ----
 
 def test_validation_es2():
     filePath = r'Avv/tests/iqValidation/from_data/es2.xml'
-    slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
-    runIqValidation.validationExecution(filePath)
-
-
-@slash.tag('turn_in')
-def test_validation_ivs():
-    filePath = r'Avv/tests/iqValidation/from_data/ivs.xml'
     slash.logger.info("running iqValidation test, xml: {}".format(filePath), extra={"highlight": True})
     runIqValidation.validationExecution(filePath)
 
@@ -41,4 +50,6 @@ def test_validation_d4m_camera():
 
 
 if __name__ == "__main__":
-    test_validation_turn_in()
+    test_validation_turn_in_mat()
+    test_validation_turn_in_ivs()
+    test_validation_turn_in_bin()
