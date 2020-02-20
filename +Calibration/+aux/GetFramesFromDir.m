0bbfbc44adeeb2349bc332b2c4e0594e2e9d94a2
syaeli <steve.yaeli@intel.com>
2020-02-20 10:55:35 +0200

    GetFrameFromDir generalization

--- +Calibration/+aux/GetFramesFromDir.m
+++ +Calibration/+aux/GetFramesFromDir.m
@@ -1,20 +1,28 @@
-function im = GetFramesFromDir(path,width, hight,type)
+function im = GetFramesFromDir(path,width, height,type)
     if(~exist('type','var'))
         type = 'I';
     end
-    if (type =='Z')
+    if strcmp(type, 'Z')
         files = dirFiles(path,'Z*.bin');
         n = length(files);
-        im = uint16(zeros(hight,width,n));
+        im = uint16(zeros(height,width,n));
         for i=1:n
-            im(:,:,i) = reshape(typecast(readAllBytes(cell2str(files(i))),'uint16'),hight,width); 
+            im(:,:,i) = reshape(typecast(readAllBytes(cell2str(files(i))),'uint16'),height,width); 
         end
-    else % no type default I
+    elseif strcmp(type, 'I')
         files = dirFiles(path,'I*.bin');
         n = length(files);
-        im = uint8(zeros(hight,width,n));
+        im = uint8(zeros(height,width,n));
         for i=1:n
-            im(:,:,i) = reshape(readAllBytes(cell2str(files(i))),hight,width); 
+            im(:,:,i) = reshape(readAllBytes(cell2str(files(i))),height,width); 
+        end
+    elseif strcmp(type, 'YUY2')
+        files = dirFiles(path,'YUY2*.bin');
+        n = length(files);
+        im = uint8(zeros(height,width,n));
+        for i=1:n
+            fprintf('%d\n',i);
+            im(:,:,i) = du.formats.readBinRGBImage(cell2str(files(i)), [width height], 5);
         end
     end
 
