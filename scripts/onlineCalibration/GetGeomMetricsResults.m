692686aef2d5032adc42cf70b6de82d6501b961f
syaeli <steve.yaeli@intel.com>
2020-04-19 23:30:47 +0300

    Unifying 2D+3D line fit metrics

--- scripts/onlineCalibration/GetGeomMetricsResults.m
+++ scripts/onlineCalibration/GetGeomMetricsResults.m
@@ -19,9 +19,9 @@ function metrics = GetGeomMetricsResults(vertices, params)
     metrics.lineFit2DHorzMax = allRes.lineFitMaxErrorTotalHoriz2D;
     metrics.lineFit2DVertRms = allRes.lineFitMeanRmsErrorTotalVertic2D;
     metrics.lineFit2DVertMax = allRes.lineFitMaxErrorTotalVertic2D;
-    metrics.lineFit3DHorzRms = allRes.lineFitRmsErrorTotal_hAF;
-    metrics.lineFit3DHorzMax = allRes.lineFitMaxErrorTotal_hAF;
-    metrics.lineFit3DVertRms = allRes.lineFitRmsErrorTotal_vAF;
-    metrics.lineFit3DVertMax = allRes.lineFitMaxErrorTotal_vAF;
+    metrics.lineFit3DHorzRms = allRes.lineFit3DRmsErrorTotal_hAF;
+    metrics.lineFit3DHorzMax = allRes.lineFit3DMaxErrorTotal_hAF;
+    metrics.lineFit3DVertRms = allRes.lineFit3DRmsErrorTotal_vAF;
+    metrics.lineFit3DVertMax = allRes.lineFit3DMaxErrorTotal_vAF;
     
 end
