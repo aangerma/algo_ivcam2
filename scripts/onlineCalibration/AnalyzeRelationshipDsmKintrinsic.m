aa4331e6fc132a9092f92e81aff103a120c377ec
syaeli <steve.yaeli@intel.com>
2020-04-21 14:54:48 +0300

    fixing RGB thermal table size in reading from unit

--- scripts/onlineCalibration/AnalyzeRelationshipDsmKintrinsic.m
+++ scripts/onlineCalibration/AnalyzeRelationshipDsmKintrinsic.m
@@ -20,21 +20,57 @@ regs.DEST.baseline2 = regs.DEST.baseline^2;
 sz = [768,1024];
 origK = [1024/2/tand(35), 0, 1024/2; 0, 768/2/tand(27), 768/2; 0, 0, 1]; % an "ideal" intrinsic matrix for XGA
 
-[yPixMat, xPixMat] = ndgrid(1:sz(1), 1:sz(2));
-xPix = xPixMat(:);
-yPix = yPixMat(:);
-verticesPix = [xPix, yPix, ones(prod(sz),1)]';
-vertices = inv(origK) * verticesPix;
+[yy, xx] = ndgrid(1:sz(1), 1:sz(2));
+in.vertices = [xx(:), yy(:), ones(prod(sz),1)] * inv(origK)';
+tic
+fprintf('Generating angles2pixels mapping...\n');
+out = Utils.convert.SphericalToCartesian(in, regs, 'inverse');
+xPixInterpolant = scatteredInterpolant(double(out.angx), double(out.angy), xx(:), 'linear');
+yPixInterpolant = scatteredInterpolant(double(out.angx), double(out.angy), yy(:), 'linear');
+toc
 
-out = Utils.convert.SphericalToCartesian(struct('vertices',vertices), regs, 'inverse');
-origAngX = out.angx;
-origAngY = out.angy;
-xPixInterpolant = scatteredInterpolant(origAngX, origAngY, xPix, 'linear');
-yPixInterpolant = scatteredInterpolant(origAngX, origAngY, YPix, 'linear');
+%% setting original pixels
+dPix = 20;
+roi = [1, 1]; % [y, x]
+
+margin = (1-roi)/2.*sz;
+x = 1+margin(2):dPix:sz(2)-margin(2);
+y = 1+margin(1):dPix:sz(1)-margin(1);
+[yy, xx] = ndgrid(y, x);
+origPixX = xx(:);
+origPixY = yy(:);
+
+in.vertices = [origPixX, origPixY, ones(length(x)*length(y),1)] * inv(origK)';
+out = Utils.convert.SphericalToCartesian(in, regs, 'inverse');
+origAngX = double(out.angx);
+origAngY = double(out.angy);
 
 %% applying angular error
-angX = 1.05*origAngX;
+angX = 1.02*origAngX;
 angY = origAngY;
+pixX = xPixInterpolant(angX, angY);
+pixY = yPixInterpolant(angX, angY);
+
+% figure, hold on
+% plot(origPixX, origPixY, 'o')
+% plot(pixX, pixY, 'o')
+% grid on
+% 
+% figure
+% plot(origPixX, pixX-origPixX, 'o')
+% grid on
+
+%% optimizing K
+Vmat = [in.vertices(:,[1,3]), zeros(size(in.vertices,1),2); zeros(size(in.vertices,1),2), in.vertices(:,[2,3])];
+Pmat = [pixX; pixY];
+Kmat = (Vmat'*Vmat)\Vmat'*Pmat;
+K = [Kmat(1), 0, Kmat(2); 0, Kmat(3), Kmat(4); 0, 0, 1];
+
+
+
+
+
+
 
 
 
