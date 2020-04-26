fb10c3ac35c94e7775d37d4a00dd00a79a9b2b2b
syaeli <steve.yaeli@intel.com>
2020-04-26 10:33:08 +0300

    Supporting AC table in reading from unit

--- +Calibration/+tables/readCalibDataFromUnit.m
+++ +Calibration/+tables/readCalibDataFromUnit.m
@@ -3,50 +3,68 @@ function [calibDataEeprom, calibDataFlash] = readCalibDataFromUnit(hw, tableName
 %   Retrieves calibration data of all algo tables from unit's EEPROM and FLASH.
 %   This version is compatible with TOC 109.
 
-tableInfo = {'Algo_Calibration_Info_CalibInfo',                 '13',   '200';...
-             'Algo_rtdOverAngX_CalibInfo',                      '15',   '80';... % max size 100
-             'Algo_Thermal_Loop_CalibInfo',                     'D',    '1F0';... % max size 200
-             'Algo_Thermal_Loop_Extra_CalibInfo',               '16',   '70';... % max size 100
-             'CBUF_Calibration_Info_CalibInfo',                 '14',   '200';...
-             'DEST_txPWRpd_Info_CalibInfo',                     '29',   '11C';... % max size 200
-             'DIGG_Gamma_Info_CalibInfo',                       '30',   'D4';... % max size 200
-             'DIGG_Undist_Info_1_CalibInfo',                    '40',   '1000';...
-             'DIGG_Undist_Info_2_CalibInfo',                    '41',   '1000';...
-             'Dynamic_Range_Info_CalibInfo',                    'F',    '100';...
-             %'FRMW_tmpTrans_Info',                              'XXX',  'C30';... %TODO: complete
-             'MEMS_Electro_Optics_Calibration_Info_CalibInfo',  '4',    '80';...
-             'RGB_Calibration_Info_CalibInfo',                  '10',   '80';...
-             'RGB_Thermal_Info_CalibInfo',                      '17',   '1F0'}; % max size 200
+%                    Table Name                                     Table Id   Table Size [bytes]
+eepromTablesInfo = {'Algo_Calibration_Info_CalibInfo',                 '13',   '200';...
+                    'Algo_rtdOverAngX_CalibInfo',                      '15',   '80';... % max size 100
+                    'Algo_Thermal_Loop_CalibInfo',                     'D',    '1F0';... % max size 200
+                    'Algo_Thermal_Loop_Extra_CalibInfo',               '16',   '70';... % max size 100
+                    'CBUF_Calibration_Info_CalibInfo',                 '14',   '200';...
+                    'DEST_txPWRpd_Info_CalibInfo',                     '29',   '11C';... % max size 200
+                    'DIGG_Gamma_Info_CalibInfo',                       '30',   'D4';... % max size 200
+                    'DIGG_Undist_Info_1_CalibInfo',                    '40',   '1000';...
+                    'DIGG_Undist_Info_2_CalibInfo',                    '41',   '1000';...
+                    'Dynamic_Range_Info_CalibInfo',                    'F',    '100';...
+                    %'FRMW_tmpTrans_Info',                              'XXX',  'C30';... %TODO: complete
+                    'MEMS_Electro_Optics_Calibration_Info_CalibInfo',  '4',    '80';...
+                    'RGB_Calibration_Info_CalibInfo',                  '10',   '80';...
+                    'RGB_Thermal_Info_CalibInfo',                      '17',   '1F0'}; % max size 200
+%                   Table Name               Table Id   Table Size [bytes]                
+flashTablesInfo = {'Algo_AutoCalibration',      '240',  '40';...
+                   'AutoCalibration_Depth_DB',  '241',  '400';... % max size 1000
+                   'AutoCalibration_RGB_DB',    '242',  '400'}; % max size 1000
          
 if exist('tableName', 'var')
-    tableInd = find(strcmp(tableInfo(:,1), tableName));
-    if isempty(tableInd)
+    eepromTableInd = find(strcmp(eepromTablesInfo(:,1), tableName));
+    flashTableInd = find(strcmp(flashTablesInfo(:,1), tableName));
+    if isempty(eepromTableInd) && isempty(flashTableInd)
         error('tableName does not match any familiar table name.')
     else
-        tableInfo = tableInfo(tableInd,:);
+        eepromTablesInfo = eepromTablesInfo(eepromTableInd,:);
+        flashTablesInfo = flashTablesInfo(flashTableInd,:);
     end
 end
 
-headerSize = 16;
-for iTable = 1:size(tableInfo, 1)
-    tableIdEeprom = tableInfo{iTable,2};
+headerSize = 16; % 0x10
+for iTable = 1:size(eepromTablesInfo, 1)
+    tableIdEeprom = eepromTablesInfo{iTable,2};
     isPayload0 = strcmp(tableIdEeprom, '4');
-    if isPayload0
-        bytesToSkip = 0;
-    else
-        bytesToSkip = headerSize;
-    end
-    tableSize = hex2dec(tableInfo{iTable,3})-headerSize;
-    calibDataEeprom(iTable).tableName = tableInfo{iTable,1};
+    bytesToSkip = headerSize*(1-isPayload0);
+    tableSize = hex2dec(eepromTablesInfo{iTable,3})-headerSize;
+    calibDataEeprom(iTable).tableName = eepromTablesInfo{iTable,1};
     [~, binData] = hw.cmd(sprintf('ReadFullTable %s', tableIdEeprom));
     binData = binData(bytesToSkip + (1:tableSize));
     calibDataEeprom(iTable).tableData = Calibration.tables.convertBinTableToCalibData(binData, calibDataEeprom(iTable).tableName);
     if ~isPayload0
         tableIdFlash = dec2hex(hex2dec(tableIdEeprom) + hex2dec('300'));
-        calibDataFlash(iTable).tableName = tableInfo{iTable,1};
+        calibDataFlash(iTable).tableName = eepromTablesInfo{iTable,1};
         [~, binData] = hw.cmd(sprintf('ReadFullTable %s', tableIdFlash));
         binData = binData(bytesToSkip + (1:tableSize));
         calibDataFlash(iTable).tableData = Calibration.tables.convertBinTableToCalibData(binData, calibDataFlash(iTable).tableName);
+    end
+end
+for iTable = 1:size(flashTablesInfo, 1)
+    tableIdFlash = flashTablesInfo{iTable,2};
+    bytesToSkip = headerSize;
+    tableSize = hex2dec(flashTablesInfo{iTable,3})-headerSize;
+    calibDataFlash(iTable).tableName = flashTablesInfo{iTable,1};
+    try
+        [~, binData] = hw.cmd(sprintf('READ_TABLE %s 0', tableIdFlash));
+        tableSize = min(tableSize, length(binData)-bytesToSkip);
+        binData = binData(bytesToSkip + (1:tableSize));
+        calibDataFlash(iTable).tableData = Calibration.tables.convertBinTableToCalibData(binData, calibDataFlash(iTable).tableName);
+    catch
+        warning('Unable to retrieve table %s', flashTablesInfo{iTable,1});
+        calibDataFlash(iTable).tableData = [];
     end
 end
 
